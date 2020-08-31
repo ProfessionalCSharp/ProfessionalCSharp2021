6be96cb0accce066c0775f3b674cb54f2bd8e6fc
Christian Nagel <christian@christiannagel.com>
2020-08-31 17:14:18 +0200

    top-level statements #18

--- 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/ExceptionFilters/Program.cs
+++ 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/ExceptionFilters/Program.cs
@@ -1,31 +1,23 @@
 ﻿using System;
+using ExceptionFilters;
 
-namespace ExceptionFilters
+try
 {
-    class Program
-    {
-        static void Main()
-        {
-            try
-            {
-                ThrowWithErrorCode(405);
+    ThrowWithErrorCode(405);
 
-            }
-            catch (MyCustomException ex) when (ex.ErrorCode == 405)
-            {
-                Console.WriteLine($"Exception caught with filter {ex.Message} and {ex.ErrorCode}");
-            }
-            catch (MyCustomException ex)
-            {
-                Console.WriteLine($"Exception caught {ex.Message} and {ex.ErrorCode}");
-            }
+}
+catch (MyCustomException ex) when (ex.ErrorCode == 405)
+{
+    Console.WriteLine($"Exception caught with filter {ex.Message} and {ex.ErrorCode}");
+}
+catch (MyCustomException ex)
+{
+    Console.WriteLine($"Exception caught {ex.Message} and {ex.ErrorCode}");
+}
 
-            Console.ReadLine();
-        }
+Console.ReadLine();
 
-        public static void ThrowWithErrorCode(int code)
-        {
-            throw new MyCustomException("Error in Foo") { ErrorCode = code };
-        }
-    }
+static void ThrowWithErrorCode(int code)
+{
+    throw new MyCustomException("Error in Foo") { ErrorCode = code };
 }
--- 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/SolicitColdCall/Program.cs
+++ 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/SolicitColdCall/Program.cs
@@ -17,7 +17,6 @@ namespace SolicitColdCall
                 ColdCallFileReaderLoop2(fileName);
                 Console.WriteLine();
             }
-
             Console.ReadLine();
         }
 
