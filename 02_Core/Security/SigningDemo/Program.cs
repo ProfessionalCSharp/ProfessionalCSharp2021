1092a034dac65a84f684c459626bd44925a8c465
Christian Nagel <christian@christiannagel.com>
2020-09-03 18:31:02 +0200

    target-typed new #15

--- 02_Core/Security/SigningDemo/Program.cs
+++ 02_Core/Security/SigningDemo/Program.cs
@@ -2,61 +2,41 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace SigningDemo
+(var aliceKeys, var alicePublicKey) = GetAliceKeys();
+
+byte[] aliceData = Encoding.UTF8.GetBytes("Alice");
+byte[] aliceSignature = CreateSignature(aliceData, aliceKeys);
+Console.WriteLine($"Alice created signature: {Convert.ToBase64String(aliceSignature)}");
+
+if (VerifySignature(aliceData, aliceSignature, alicePublicKey))
 {
-    class Program
+    Console.WriteLine("Alice signature verified successfully");
+}
+
+(CngKey KeyPair, byte[] PublicKey) GetAliceKeys()
+{
+    var aliceKeyPair = CngKey.Create(CngAlgorithm.ECDsaP521);
+    var alicePublicKey = aliceKeyPair.Export(CngKeyBlobFormat.GenericPublicBlob);
+    return (aliceKeyPair, alicePublicKey); 
+}
+
+byte[] CreateSignature(byte[] data, CngKey key)
+{
+    byte[] signature;
+    using (var signingAlg = new ECDsaCng(key))
     {
-
-        CngKey _aliceKeySignature;
-        private byte[] _alicePubKeyBlob;
-
-        static void Main()
-        {
-            var p = new Program();
-            p.Run();
-        }
-
-        public void Run()
-        {
-            InitAliceKeys();
-
-            byte[] aliceData = Encoding.UTF8.GetBytes("Alice");
-            byte[] aliceSignature = CreateSignature(aliceData, _aliceKeySignature);
-            Console.WriteLine($"Alice created signature: {Convert.ToBase64String(aliceSignature)}");
-
-            if (VerifySignature(aliceData, aliceSignature, _alicePubKeyBlob))
-            {
-                Console.WriteLine("Alice signature verified successfully");
-            }
-        }
-
-        public void InitAliceKeys()
-        {
-            _aliceKeySignature = CngKey.Create(CngAlgorithm.ECDsaP521);
-            _alicePubKeyBlob = _aliceKeySignature.Export(CngKeyBlobFormat.GenericPublicBlob);
-        }
-
-        private byte[] CreateSignature(byte[] data, CngKey key)
-        {
-            byte[] signature;
-            using (var signingAlg = new ECDsaCng(key))
-            {
-                signature = signingAlg.SignData(data, HashAlgorithmName.SHA512);
-                signingAlg.Clear();
-            }
-            return signature;
-        }
-
-        private bool VerifySignature(byte[] data, byte[] signature, byte[] pubKey)
-        {
-            bool retValue = false;
-            using (CngKey key = CngKey.Import(pubKey, CngKeyBlobFormat.GenericPublicBlob))
-            using (var signingAlg = new ECDsaCng(key))
-            {
-                retValue = signingAlg.VerifyData(data, signature, HashAlgorithmName.SHA512);
-                signingAlg.Clear();
-            }
-            return retValue;
-        }
+        signature = signingAlg.SignData(data, HashAlgorithmName.SHA512);
+        signingAlg.Clear();
     }
+    return signature;
+}
+
+bool VerifySignature(byte[] data, byte[] signature, byte[] pubKey)
+{
+    bool retValue = false;
+    using CngKey key = CngKey.Import(pubKey, CngKeyBlobFormat.GenericPublicBlob);
+    using var signingAlg = new ECDsaCng(key);
+    retValue = signingAlg.VerifyData(data, signature, HashAlgorithmName.SHA512);
+    signingAlg.Clear();
+    return retValue;
 }
