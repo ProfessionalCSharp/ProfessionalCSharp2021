e265f4f640e46afa4e542de589504c80e195d663
Christian Nagel <christian@christiannagel.com>
2020-09-07 15:08:49 +0200

    top-level statements

--- 02_Core/SpecialCollections/SpecialCollections/ObservableCollectionSample/Program.cs
+++ 02_Core/SpecialCollections/SpecialCollections/ObservableCollectionSample/Program.cs
@@ -2,48 +2,40 @@
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
-namespace ObservableCollectionSample
+var data = new ObservableCollection<string>();
+
+data.CollectionChanged += Data_CollectionChanged;
+data.Add("One");
+data.Add("Two");
+data.Insert(1, "Three");
+data.Remove("One");
+
+data.CollectionChanged -= Data_CollectionChanged;
+
+Console.ReadLine();
+
+
+void Data_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
 {
-    class Program
+    Console.WriteLine($"action: {e.Action}");
+
+    if (e.OldItems != null)
     {
-        static void Main()
+        Console.WriteLine($"starting index for old item(s): {e.OldStartingIndex}");
+        Console.WriteLine("old item(s):");
+        foreach (var item in e.OldItems)
         {
-            var data = new ObservableCollection<string>();
-            
-            data.CollectionChanged += Data_CollectionChanged;
-            data.Add("One");
-            data.Add("Two");
-            data.Insert(1, "Three");
-            data.Remove("One");
-
-            data.CollectionChanged -= Data_CollectionChanged;
-
-            Console.ReadLine();
-        }
-
-        public static void Data_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            Console.WriteLine($"action: {e.Action.ToString()}");
-
-            if (e.OldItems != null)
-            {
-                Console.WriteLine($"starting index for old item(s): {e.OldStartingIndex}");
-                Console.WriteLine("old item(s):");
-                foreach (var item in e.OldItems)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-            if (e.NewItems != null)
-            {
-                Console.WriteLine($"starting index for new item(s): {e.NewStartingIndex}");
-                Console.WriteLine("new item(s): ");
-                foreach (var item in e.NewItems)
-                {
-                    Console.WriteLine(item);
-                }
-            }
-            Console.WriteLine();
+            Console.WriteLine(item);
         }
     }
+    if (e.NewItems != null)
+    {
+        Console.WriteLine($"starting index for new item(s): {e.NewStartingIndex}");
+        Console.WriteLine("new item(s): ");
+        foreach (var item in e.NewItems)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    Console.WriteLine();
 }
