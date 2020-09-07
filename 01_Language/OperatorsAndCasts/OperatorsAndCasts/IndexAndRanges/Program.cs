ec10eb3e6bc089f9176dbcd64fa0a70e2ec8b626
Christian Nagel <christian@christiannagel.com>
2020-09-07 15:12:34 +0200

    indexes and ranges #22

---
+++ 01_Language/OperatorsAndCasts/OperatorsAndCasts/IndexAndRanges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+var ix3 = new Index(3);
+var ixend3 = new Index(3, fromEnd: true);
+
+Index ix4 = 4;
+Index ixend4 = ^4;
+
+var range1 = 3..5;
+var range2 = new Range(ix3, ix4);
+var range3 = Range.StartAt(4);
+(var offset, var length) = range3.GetOffsetAndLength(10);
