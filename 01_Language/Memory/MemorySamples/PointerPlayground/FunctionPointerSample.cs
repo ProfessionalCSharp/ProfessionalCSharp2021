b131fc85e41fb2d833b9758de7405454e39db2d3
Christian Nagel <christian@christiannagel.com>
2020-10-08 13:03:18 +0200

    folder reorg

--- 01_Language/Memory/MemorySamples/PointerPlayground/FunctionPointerSample.cs
+++ 01_Language/Memory/MemorySamples/PointerPlayground/FunctionPointerSample.cs
@@ -3,8 +3,14 @@
 namespace PointerPlayground
 {
     unsafe class FunctionPointerSample
-    {
+    {      
         public static void Calc(delegate* managed<int, int, int> func)
+        {
+            int result = func(42, 11);
+            Console.WriteLine($"function pointer result: {result}");
+        }
+
+        public static void CalcUnmanaged(delegate* unmanaged[Stdcall]<int, int, int> func)
         {
             int result = func(42, 11);
             Console.WriteLine($"function pointer result: {result}");
--- 01_Language/Memory/MemorySamples/PointerPlayground/Program.cs
+++ 01_Language/Memory/MemorySamples/PointerPlayground/Program.cs
@@ -32,11 +32,18 @@ namespace PointerPlayground
             FunctionPointerSample.Calc(&Add);
             FunctionPointerSample.Calc(&Subtract);
 
+            //delegate* managed<int, int, int>[] arrayOfPointers = new delegate* managed<int, int, int>[3];
+            //arrayOfPointers[0] = &Add;
+            //arrayOfPointers[1] = &Subtract;
+            //delegate* managed<int, int, int> first = arrayOfPointers[0];
+            //int result = first(4, 3);
+            //Console.WriteLine(result);
+            //first += 1;
+
+
             Console.ReadLine();
         }
-
-        
-
+       
         static int Add(int x, int y) => x + y;
         static int Subtract(int x, int y) => x - y;
     }
