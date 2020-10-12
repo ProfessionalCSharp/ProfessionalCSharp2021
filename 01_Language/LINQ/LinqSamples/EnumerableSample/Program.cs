8f4ad6960c1574d83f30fce86d95d690950c6f11
Christian Nagel <christian@christiannagel.com>
2020-10-12 22:33:42 +0200

    updates for System.CommandLine

--- 01_Language/LINQ/LinqSamples/EnumerableSample/Program.cs
+++ 01_Language/LINQ/LinqSamples/EnumerableSample/Program.cs
@@ -1,47 +1,12 @@
 ﻿using System.CommandLine;
+using EnumerableSample;
 
-namespace EnumerableSample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var rootCommand = new RootCommand("EnumerableSample");
+var rootCommand = new RootCommand("EnumerableSample");
 
-            RegisterCommandHandler.Register(rootCommand, "linq", nameof(LinqSamples));
-            RegisterCommandHandler.Register(rootCommand, "filter", nameof(FilteringSamples));
-            RegisterCommandHandler.Register(rootCommand, "grouping", nameof(GroupingSamples));
-            RegisterCommandHandler.Register(rootCommand, "compound", nameof(CompoundFromSamples));
-            RegisterCommandHandler.Register(rootCommand, "join", nameof(JoinSamples));
-            RegisterCommandHandler.Register(rootCommand, "sort", nameof(SortingSamples));
-            rootCommand.Invoke(args);
-            //var app = new CommandLineApplication(throwOnUnexpectedArg: false);
-            //app.FullName = "LINQ Sample App";
-            //LinqSamples.Register(app);
-            //FilteringSamples.Register(app);
-            //GroupingSamples.Register(app);
-            //CompoundFromSamples.Register(app);
-            //JoinSamples.Register(app);
-            //SortingSamples.Register(app);
-
-            //app.Command("help", cmd =>
-            //{
-            //    cmd.Description = "Get help for the application";
-            //    CommandArgument commandArgument = cmd.Argument("<COMMAND>", "The command to get help for");
-            //    cmd.OnExecute(() =>
-            //    {
-            //        app.ShowHelp(commandArgument.Value);
-            //        return 0;
-            //    });
-            //});
-
-            //app.OnExecute(() =>
-            //{
-            //    app.ShowHelp();
-            //    return 0;
-            //});
-
-            //app.Execute(args);
-        }
-    }
-}
+RegisterCommandHandler.Register(rootCommand, "linq", nameof(LinqSamples));
+RegisterCommandHandler.Register(rootCommand, "filter", nameof(FilteringSamples));
+RegisterCommandHandler.Register(rootCommand, "grouping", nameof(GroupingSamples));
+RegisterCommandHandler.Register(rootCommand, "compound", nameof(CompoundFromSamples));
+RegisterCommandHandler.Register(rootCommand, "join", nameof(JoinSamples));
+RegisterCommandHandler.Register(rootCommand, "sort", nameof(SortingSamples));
+rootCommand.Invoke(args);
