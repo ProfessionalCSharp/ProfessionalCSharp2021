cf4d9b9eb1ac60f4fb136423f8a318180a6db60b
Christian Nagel <christian@christiannagel.com>
2020-10-14 21:37:55 +0200

    cleanup

--- 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/CallerInformation/Program.cs
+++ 01_Language/ErrorsAndExceptions/ErrorsAndExceptions/CallerInformation/Program.cs
@@ -1,38 +1,35 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace CallerInformation
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
-        {
-            var p = new Program();
-            p.Log();
-            p.SomeProperty = 33;
-            Action a1 = () => p.Log();
-            a1();
-        }
+        var p = new Program();
+        p.Log();
+        p.SomeProperty = 33;
+        Action a1 = () => p.Log();
+        a1();
+    }
 
-        private int _someProperty;
-        public int SomeProperty
+    private int _someProperty;
+    public int SomeProperty
+    {
+        get => _someProperty;
+        set
         {
-            get => _someProperty;
-            set
-            {
-                Log();
-                _someProperty = value;
-            }
+            Log();
+            _someProperty = value;
         }
+    }
 
-        public void Log([CallerLineNumber] int line = -1,
-            [CallerFilePath] string? path = null,
-            [CallerMemberName] string? name = null)
-        {
-            Console.WriteLine((line < 0) ? "No line" : "Line " + line);
-            Console.WriteLine((path == null) ? "No file path" : path);
-            Console.WriteLine((name == null) ? "No member name" : name);
-            Console.WriteLine();
-        }
+    public void Log([CallerLineNumber] int line = -1,
+        [CallerFilePath] string? path = null,
+        [CallerMemberName] string? name = null)
+    {
+        Console.WriteLine((line < 0) ? "No line" : "Line " + line);
+        Console.WriteLine((path == null) ? "No file path" : path);
+        Console.WriteLine((name == null) ? "No member name" : name);
+        Console.WriteLine();
     }
 }
