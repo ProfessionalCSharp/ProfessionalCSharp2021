fd460d3a93a79a1d5d43d6a78eeccbcae198ba66
Christian Nagel <christian@christiannagel.com>
2020-10-19 08:56:53 +0200

    configuration samples

---
+++ 02_Libs/DependencyInjectionAndConfiguration/ConfigurationSamples/ConfigurationSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ConfigurationSample;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+using var host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(config =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("customconfigurationfile.json", optional: true);
+
+    }).ConfigureServices(services =>
+    {
+        services.AddTransient<SampleService>();
+        services.AddTransient<SampleService2>();
+    }).Build();
+
+var service = host.Services.GetRequiredService<SampleService>();
+service.ShowConfiguration();
+service.ShowTypedConfiguration();
+service.ShowCustomConfiguration();
+
+var service2 = host.Services.GetRequiredService<SampleService2>();
+service2.ShowHostEnvironment();
+
---
+++ 02_Libs/DependencyInjectionAndConfiguration/ConfigurationSamples/ConfigurationSample/SampleService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConfigurationSample
+{
+    public class InnerConfig
+    {
+        public string Key5 { get; set; }
+    }
+
+    public class TypedConfig
+    {
+        public string Key3 { get; set; }
+        public string Key4 { get; set; }
+        public InnerConfig InnerConfig { get; set; }
+
+        public override string ToString() =>
+            $"values: {Key3} {Key4} {InnerConfig.Key5}";
+    }
+
+    public class SampleService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SampleService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ShowConfiguration()
+        {
+            string value1 = _configuration.GetValue<string>("Key1");
+            Console.WriteLine(value1);
+            string value1b = _configuration["Key1"];
+            Console.WriteLine(value1b);
+            string value2 = _configuration.GetSection("Section1")["Key2"];
+            Console.WriteLine(value2);
+            string connectionString = _configuration.GetConnectionString("BooksConnection");
+            Console.WriteLine(connectionString);
+            Console.WriteLine();
+
+        }
+
+        public void ShowTypedConfiguration()
+        {
+            Console.WriteLine(nameof(ShowTypedConfiguration));
+            var section = _configuration.GetSection("SomeTypedConfig");
+            var typedConfig = section.Get<TypedConfig>(binder => binder.BindNonPublicProperties = true);
+            Console.WriteLine(typedConfig);
+            Console.WriteLine();
+        }
+
+        public void ShowCustomConfiguration()
+        {
+            Console.WriteLine(nameof(ShowCustomConfiguration));
+            var value = _configuration["KeyCustom1"];
+            Console.WriteLine(value);
+            Console.WriteLine();
+        }
+    }
+}
