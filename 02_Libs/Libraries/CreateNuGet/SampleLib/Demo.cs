55b9ab99b33d1ee4fc258110d9da5ddcc83da65b
Christian Nagel <christian@christiannagel.com>
2020-10-22 08:54:38 +0200

    update lib for .net 5

--- 02_Libs/Libraries/CreateNuGet/SampleLib/Demo.cs
+++ 02_Libs/Libraries/CreateNuGet/SampleLib/Demo.cs
@@ -2,14 +2,15 @@
 {
     public class Demo
     {
-#if NETSTANDARD2_0
+#if NET50
+        private static string s_info = ".NET 5.0";
+#elif NETSTANDARD2_0
         private static string s_info = ".NET Standard 2.0";
 #elif DOTNET47
         private static string s_info = ".NET 4.7";
 #else
         private static string s_info = "Unknown";
 #endif
-
         public static string Show() => s_info;
     }
 }
