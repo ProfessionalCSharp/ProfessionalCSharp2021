8a3d8b5f3422b55ce0625cbe065e9f3afb7ca333
Christian Nagel <christian@christiannagel.com>
2020-11-19 20:29:53 +0100

    change group command

--- 01_CS/LINQ/LinqSamples/EnumerableSample/Program.cs
+++ 01_CS/LINQ/LinqSamples/EnumerableSample/Program.cs
@@ -5,7 +5,7 @@ var rootCommand = new RootCommand("EnumerableSample");
 
 RegisterCommands.Register(rootCommand, "linq", nameof(LinqSamples));
 RegisterCommands.Register(rootCommand, "filter", nameof(FilterSamples));
-RegisterCommands.Register(rootCommand, "grouping", nameof(GroupSamples));
+RegisterCommands.Register(rootCommand, "group", nameof(GroupSamples));
 RegisterCommands.Register(rootCommand, "compound", nameof(CompoundFromSamples));
 RegisterCommands.Register(rootCommand, "join", nameof(JoinSamples));
 RegisterCommands.Register(rootCommand, "sort", nameof(SortingSamples));
