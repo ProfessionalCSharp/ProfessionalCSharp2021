5d4f796566b8eb258623e45b736145bd70012582
Christian Nagel <christian@christiannagel.com>
2020-11-20 09:40:12 +0100

    move bitarray and bitvector samples to arrays (from special collections)

---
+++ 01_CS/Arrays/ArraysSamples/BitArraySample/BitArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Text;
+
+public static class BitArrayExtensions
+{
+    public static string FormatString(this BitArray bits)
+    {
+        StringBuilder sb = new();
+        for (int i = bits.Length - 1; i >= 0; i--)
+        {
+            sb.Append(bits[i] ? 1 : 0);
+            if (i != 0 && i % 4 == 0)
+            {
+                sb.Append("_");
+            }
+        }
+
+        return sb.ToString();
+    }
+}
+
---
+++ 01_CS/Arrays/ArraysSamples/BitArraySample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+BitArray bits1 = new(9);
+bits1.SetAll(true);
+bits1.Set(1, false);
+bits1[5] = false;
+bits1[7] = false;
+Console.Write("initialized: ");
+Console.WriteLine(bits1.FormatString());
+
+Console.Write("not ");
+Console.Write(bits1.FormatString());
+bits1.Not();
+Console.Write(" = ");
+Console.WriteLine(bits1.FormatString());
+
+BitArray bits2 = new(bits1);
+bits2[0] = true;
+bits2[1] = false;
+bits2[4] = true;
+Console.Write($"{bits1.FormatString()} OR {bits2.FormatString()}");
+Console.Write(" = ");
+bits1.Or(bits2);
+Console.WriteLine(bits1.FormatString());
+
+Console.Write($"{bits2.FormatString()} AND {bits1.FormatString()}");
+Console.Write(" = ");
+bits2.And(bits1);
+Console.WriteLine(bits2.FormatString());
+
+Console.Write($"{bits1.FormatString()} XOR {bits2.FormatString()}");
+bits1.Xor(bits2);
+Console.Write(" = ");
+Console.WriteLine(bits1.FormatString());
+
+Console.ReadLine();
