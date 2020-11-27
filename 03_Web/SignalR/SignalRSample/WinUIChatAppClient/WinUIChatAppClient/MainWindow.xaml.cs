1b479f8534a4b3d522e52dcf1df1c4e5ae1855a6
Christian Nagel <christian@christiannagel.com>
2020-11-27 22:42:35 +0100

    initiali winui chat client

---
+++ 03_Web/SignalR/SignalRSample/WinUIChatAppClient/WinUIChatAppClient/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ChatClient.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUIChatAppClient
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+
+        private IServiceScope? _serviceScope;
+        public MainWindow()
+        {
+            _serviceScope = (Application.Current as App)!.Services.CreateScope();
+            ViewModel = _serviceScope.ServiceProvider.GetRequiredService<MainWindowViewModel>();
+            this.InitializeComponent();
+        }
+
+        public MainWindowViewModel ViewModel { get; }
+
+    }
+}
---
+++ 03_Web/SignalR/SignalRSample/WinUIChatAppClient/WinUIChatAppClient/Services/IDialogService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ChatClient.Services
+{
+    public interface IDialogService
+    {
+        Task ShowMessageAsync(string message);
+    }
+}
