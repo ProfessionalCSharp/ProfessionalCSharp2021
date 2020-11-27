75bf554380981fb9d34947539ac838fd5eb40de1
Christian Nagel <christian@christiannagel.com>
2020-11-27 10:37:52 +0100

    websocket - update project to .NET 5

--- 03_Web/SignalR/WebSocketsSample/WebSocketsServer/Startup.cs
+++ 03_Web/SignalR/WebSocketsSample/WebSocketsServer/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Net.WebSockets;
@@ -13,14 +14,11 @@ namespace WebSocketsServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
