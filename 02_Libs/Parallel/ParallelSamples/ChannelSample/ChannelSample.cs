2ff79d3329b0457ba17ad0c9420281d8f9f62ac6
Christian Nagel <christian@christiannagel.com>
2021-01-05 09:04:34 +0100

    System.Threading.Channels sample #39 #40

---
+++ 02_Libs/Parallel/ParallelSamples/ChannelSample/ChannelSample.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+class ChannelSample
+{
+    public static Task WriteSomeDataWithTryWriteAsync(ChannelWriter<SomeData> writer) =>
+        Task.Run(async () =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Random r = new();
+                SomeData data = new($"text {i}", i);
+                await Task.Delay(r.Next(50));
+                if (!writer.TryWrite(data))
+                {
+                    Console.WriteLine($"could not write {data.Number}, channel full");
+                }
+                else
+                {
+                    Console.WriteLine($"Written {data.Text}");
+                }
+            }
+            writer.Complete();
+            Console.WriteLine("Writing completed");
+        });
+
+    public static Task ReadSomeDataAsync(ChannelReader<SomeData> reader) =>
+        Task.Run(async () =>
+        {
+            try
+            {
+                Console.WriteLine("Start reading...");
+                Random r = new();
+                while (true)
+                {
+                    await Task.Delay(r.Next(80));
+                    var data = await reader.ReadAsync();
+                    Console.WriteLine($"read: {data.Text}, available items: {reader.Count}");
+                }
+            }
+            catch (ChannelClosedException)
+            {
+                Console.WriteLine("channel closed");
+            }
+        });
+    
+
+    public static Task WriteSomeDataAsync(ChannelWriter<SomeData> writer) =>
+        Task.Run(async () =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Random r = new();
+                SomeData data = new($"text {i}", i);
+                await Task.Delay(r.Next(50));
+                await writer.WriteAsync(data);
+                Console.WriteLine($"Written {data.Text}");
+            }
+            writer.Complete();
+            Console.WriteLine("Writing completed");
+        });    
+
+    public static Task ReadSomeDataUsingAsyncStreams(ChannelReader<SomeData> reader) =>    
+        Task.Run(async () =>
+        {
+            try
+            {
+                Console.WriteLine("Start reading...");
+                Random r = new();
+                await foreach (var data in reader.ReadAllAsync())
+                {
+                    await Task.Delay(r.Next(80));
+                    Console.WriteLine($"read: {data.Text} available items: {reader.Count}");
+                }
+
+            }
+            catch (ChannelClosedException)
+            {
+                Console.WriteLine("channel closed");
+            }
+        });
+    
+}
---
+++ 02_Libs/Parallel/ParallelSamples/ChannelSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+await UsingTheUnboundedChannelAsync();
+await UsingTheBoundedChannelAsync();
+
+async Task UsingTheUnboundedChannelAsync()
+{
+    Channel<SomeData> channel = Channel.CreateUnbounded<SomeData>(new UnboundedChannelOptions() { SingleReader = false, SingleWriter = true, });
+
+    Console.WriteLine("Using the unbounded channel");
+
+    var t1 = ChannelSample.WriteSomeDataAsync(channel.Writer);
+    var t2 = ChannelSample.ReadSomeDataAsync(channel.Reader);
+
+    await Task.WhenAll(t1, t2);
+
+    Console.WriteLine();
+}
+
+async Task UsingTheBoundedChannelAsync()
+{
+    Channel<SomeData> channel = Channel.CreateBounded<SomeData>(new BoundedChannelOptions(capacity: 10) { FullMode = BoundedChannelFullMode.Wait, SingleWriter = true });
+
+    Console.WriteLine("Using the bounded channel");
+
+    var t1 = ChannelSample.WriteSomeDataWithTryWriteAsync(channel.Writer);
+    var t2 = ChannelSample.ReadSomeDataUsingAsyncStreams(channel.Reader);
+
+    await Task.WhenAll(t1, t2);
+
+    Console.WriteLine("bye");
+    Console.WriteLine();
+}
+
+
+public record SomeData(string Text, int Number);
