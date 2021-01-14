9561ce0be335ec9dee84eef8397751c11d839b96
Christian Nagel <christian@christiannagel.com>
2021-01-14 14:20:36 +0100

    UDP sender and receiver #43

--- 02_Libs/Networking/NetworkingSamples/UdpSender/Program.cs
+++ 02_Libs/Networking/NetworkingSamples/UdpSender/Program.cs
@@ -1,169 +1,77 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
 using System.Threading.Tasks;
 
-namespace UdpSender
-{
-    class Program
+await BuildCommandLine()
+    .UseHost(_ => Host.CreateDefaultBuilder(), hostBuilder =>
     {
-        static async Task Main(string[] args)
+        hostBuilder.ConfigureServices(services =>
         {
-            if (!ParseCommandLine(args, out int port, out string? hostname, out bool broadcast, out string? groupAddress, out bool ipv6))
-            {
-                ShowUsage();
-                Console.ReadLine();
-                return;
-            }
-            IPEndPoint? endpoint = await GetIPEndPointAsync(port, hostname, broadcast, groupAddress, ipv6);
-            await SenderAsync(endpoint, broadcast, groupAddress);
-            Console.WriteLine("Press return to exit...");
-            Console.ReadLine();
-        }
+            services.AddTransient<Sender>();
+        });
+    })
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
 
-        private static string? GetValueForKey(string[] args, string key)
+
+CommandLineBuilder BuildCommandLine()
+{
+    RootCommand rootCommand = new("UdpSender");
+    var portOption = new Option<int>("--port", "Port number, enter a port number of the receiver")
+    {
+        IsRequired = true
+    };
+    portOption.AddAlias("-p");
+    var groupAddressOption = new Option<string>("--groupaddress", "Enter a group address in the range 224.0.0.0 to 239.255.255.255")
+    {
+        IsRequired = false
+    };
+    groupAddressOption.AddAlias("-g");
+    var broadcastFlag = new Option<bool>("--broadcast")
+    {
+        IsRequired = false
+    };
+    broadcastFlag.AddAlias("-b");
+    var hostNameOption = new Option<string>("--hostname", "Use the hostname if the message should be sent to a single host")
+    {
+        IsRequired = false
+    };
+    hostNameOption.AddAlias("-h");
+    var ipv6Flag = new Option<bool>("--ipv6")
+    {
+        IsRequired = false
+    };
+    rootCommand.AddOption(portOption);
+    rootCommand.AddOption(groupAddressOption);
+    rootCommand.AddOption(broadcastFlag);
+    rootCommand.AddOption(hostNameOption);
+    rootCommand.AddOption(ipv6Flag);
+    rootCommand.Handler = CommandHandler.Create<int, string?, bool, string?, bool, IHost>(RunAsync);
+    CommandLineBuilder commandLineBuilder = new(rootCommand);
+    commandLineBuilder.UseMiddleware(async (context, next) =>
+    {
+        context.Console.Out.WriteLine("my middle");
+        if (context.ParseResult.HasOption(hostNameOption) || context.ParseResult.HasOption(broadcastFlag) || context.ParseResult.HasOption(groupAddressOption))
         {
-            int? nextIndex = args.Select((a, i) => new { Arg = a, Index = i }).SingleOrDefault(a => a.Arg == key)?.Index + 1;
-            if (!nextIndex.HasValue)
-            {
-                return null;
-            }
-            return args[nextIndex.Value];
+            await next(context);
         }
-
-        private static bool ParseCommandLine(string[] args, out int port, out string? hostname, out bool broadcast, out string? groupAddress, out bool ipv6)
+        else
         {
-            port = 0;
-            hostname = string.Empty;
-            broadcast = false;
-            groupAddress = string.Empty;
-            ipv6 = false;
-            if (args.Length < 2 || args.Length > 5)
-            {
-                return false;
-            }
-            if (args.SingleOrDefault(a => a == "-p") == null)
-            {
-                Console.WriteLine("-p required");
-                return false;
-            }
-            string[] requiredOneOf = { "-h", "-b", "-g" };
-            if (args.Intersect(requiredOneOf).Count() != 1)
-            {
-                Console.WriteLine("either one (and only one) of -h -b -g required");
-                return false;
-            }
-
-            // get port number
-            string? port1 = GetValueForKey(args, "-p");
-            if (port1 == null || !int.TryParse(port1, out port))
-            {
-                return false;
-            }
-
-            // get optional host name
-            hostname = GetValueForKey(args, "-h");
-
-            broadcast = args.Contains("-b");
-
-            ipv6 = args.Contains("-ipv6");
-
-            // get optional group address
-            groupAddress = GetValueForKey(args, "-g");
-            return true;
+            context.Console.Out.WriteLine("Supply hostname, groupaddress or broadcast");
         }
+    });
+    return commandLineBuilder;
+}
 
-        private static void ShowUsage()
-        {
-            Console.WriteLine("Usage: UdpSender -p port [-g groupaddress | -b | -h hostname] [-ipv6]");
-            Console.WriteLine("\t-p port number\tEnter a port number for the sender");
-            Console.WriteLine("\t-g group address\tGroup address in the range 224.0.0.0 to 239.255.255.255");
-            Console.WriteLine("\t-b\tFor a broadcast");
-            Console.WriteLine("\t-h hostname\tUse the hostname option if the message should be sent to a single host");
-        }
-
-        public static async Task<IPEndPoint?> GetIPEndPointAsync(int port, string? hostName, bool broadcast, string? groupAddress, bool ipv6)
-        {
-            IPEndPoint? endpoint = null;
-            try
-            {
-                if (broadcast)
-                {
-                    endpoint = new IPEndPoint(IPAddress.Broadcast, port);
-                }
-                else if (hostName != null)
-                {
-                    IPHostEntry hostEntry = await Dns.GetHostEntryAsync(hostName);
-                    IPAddress? address = null;
-                    if (ipv6)
-                    {
-                        address = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetworkV6).FirstOrDefault();
-                    }
-                    else
-                    {
-                        address = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
-                    }
-
-                    if (address == null)
-                    {
-                        Func<string> ipversion = () => ipv6 ? "IPv6" : "IPv4";
-                        Console.WriteLine($"no {ipversion()} address for {hostName}");
-                        return null;
-                    }
-                    endpoint = new IPEndPoint(address, port);
-                }
-                else if (groupAddress != null)
-                {
-                    endpoint = new IPEndPoint(IPAddress.Parse(groupAddress), port);
-                }
-                else
-                {
-                    throw new InvalidOperationException($"{nameof(hostName)}, {nameof(broadcast)}, or {nameof(groupAddress)} must be set");
-                }
-            }
-            catch (SocketException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return endpoint;
-        }
-
-        private static async Task SenderAsync(IPEndPoint? endpoint, bool broadcast, string? groupAddress)
-        {
-            try
-            {
-                string localhost = Dns.GetHostName();
-                using var client = new UdpClient(new IPEndPoint(IPAddress.Parse("192.168.178.20"), 0));
-                client.EnableBroadcast = broadcast;
-                if (groupAddress != null)
-                {
-                    client.JoinMulticastGroup(IPAddress.Parse(groupAddress));
-                }
-
-                bool completed = false;
-                do
-                {
-                    Console.WriteLine("Enter a message or bye to exit");
-                    string? input = Console.ReadLine();
-                    Console.WriteLine();
-                    completed = input == "bye";
-
-                    byte[] datagram = Encoding.UTF8.GetBytes($"{input} from {localhost}");
-                    int sent = await client.SendAsync(datagram, datagram.Length, endpoint);
-                    Console.WriteLine($"Sent datagram using local EP {client.Client.LocalEndPoint} to {endpoint}");
-                } while (!completed);
-
-                if (groupAddress != null)
-                {
-                    client.DropMulticastGroup(IPAddress.Parse(groupAddress));
-                }
-            }
-            catch (SocketException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-    }
+async Task RunAsync(int port, string? hostName, bool broadcast, string? groupAddress, bool ipv6, IHost host)
+{
+    var sender = host.Services.GetRequiredService<Sender>();
+    await sender.RunAsync(port, hostName, broadcast, groupAddress, ipv6);
 }
---
+++ 02_Libs/Networking/NetworkingSamples/UdpSender/Sender.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Sender
+{
+    private readonly ILogger _logger;
+    public Sender(ILogger<Sender> logger) => _logger = logger;
+
+    private async Task<IPEndPoint?> GetIPEndPointAsync(int port, string? hostName, bool broadcast, string? groupAddress, bool ipv6)
+    {
+        IPEndPoint? endpoint = null;
+        try
+        {
+            if (broadcast)
+            {
+                endpoint = new IPEndPoint(IPAddress.Broadcast, port);
+            }
+            else if (hostName != null)
+            {
+                IPHostEntry hostEntry = await Dns.GetHostEntryAsync(hostName);
+                IPAddress? address = null;
+                if (ipv6)
+                {
+                    address = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetworkV6).FirstOrDefault();
+                }
+                else
+                {
+                    address = hostEntry.AddressList.Where(a => a.AddressFamily == AddressFamily.InterNetwork).FirstOrDefault();
+                }
+
+                if (address == null)
+                {
+                    Func<string> ipversion = () => ipv6 ? "IPv6" : "IPv4";
+                    _logger.LogWarning($"no {ipversion()} address for {hostName}");
+                    return null;
+                }
+                endpoint = new IPEndPoint(address, port);
+            }
+            else if (groupAddress != null)
+            {
+                endpoint = new IPEndPoint(IPAddress.Parse(groupAddress), port);
+            }
+            else
+            {
+                throw new InvalidOperationException($"{nameof(hostName)}, {nameof(broadcast)}, or {nameof(groupAddress)} must be set");
+            }
+        }
+        catch (SocketException ex)
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+        return endpoint;
+    }
+
+    public async Task RunAsync(int port, string? hostName, bool broadcast, string? groupAddress, bool ipv6)
+    {
+        IPEndPoint? endpoint = await GetIPEndPointAsync(port, hostName, broadcast, groupAddress, ipv6);
+        if (endpoint == null) return;
+
+        try
+        {
+            string localhost = Dns.GetHostName();
+            using UdpClient client = new(); // (new IPEndPoint(IPAddress.Parse("192.168.178.20"), 0));
+            client.EnableBroadcast = broadcast;
+            if (groupAddress != null)
+            {
+                client.JoinMulticastGroup(IPAddress.Parse(groupAddress));
+            }
+
+            bool completed = false;
+            do
+            {
+                Console.WriteLine("Enter a message or bye to exit");
+                string? input = Console.ReadLine();
+                Console.WriteLine();
+                completed = input == "bye";
+
+                byte[] datagram = Encoding.UTF8.GetBytes($"{input} from {localhost}");
+                int sent = await client.SendAsync(datagram, datagram.Length, endpoint);
+                _logger.LogInformation($"Sent datagram using local EP {client.Client.LocalEndPoint} to {endpoint}");
+            } while (!completed);
+
+            if (groupAddress != null)
+            {
+                client.DropMulticastGroup(IPAddress.Parse(groupAddress));
+            }
+        }
+        catch (SocketException ex)
+        {
+            _logger.LogError(ex, ex.Message);
+        }
+    }
+}
