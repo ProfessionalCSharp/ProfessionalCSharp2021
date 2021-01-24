966abcd6dae7af2285c76f5de61aeedf3ee27e6c
Christian Nagel <christian@christiannagel.com>
2021-01-24 14:38:48 +0100

    DNS lookup #43

--- 02_Libs/Networking/NetworkingSamples/DnsLookup/Program.cs
+++ 02_Libs/Networking/NetworkingSamples/DnsLookup/Program.cs
@@ -2,50 +2,41 @@
 using System.Net;
 using System.Threading.Tasks;
 
-namespace DnsLookup
+do
 {
-    class Program
+    Console.Write("Hostname:\t");
+    string? hostname = Console.ReadLine();
+    if (hostname is null || hostname.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
     {
-        static async Task Main()
+        Console.WriteLine("bye!");
+        return;
+    }
+    await OnLookupAsync(hostname);
+    Console.WriteLine();
+} while (true);
+
+
+async Task OnLookupAsync(string hostname)
+{
+    try
+    {
+        IPHostEntry ipHost = await Dns.GetHostEntryAsync(hostname);
+
+        Console.WriteLine($"Hostname: {ipHost.HostName}");
+
+        // Aliases not populated by GetHostEntryAsync
+        //if (ipHost.Aliases.Length != 0)
+        //{
+        //    Console.WriteLine($"Aliases: {string.Join(", ", ipHost.Aliases)}");
+        //}
+
+        foreach (IPAddress address in ipHost.AddressList)
         {
-            do
-            {
-                Console.Write("Hostname:\t");
-                string? hostname = Console.ReadLine();
-                if (hostname?.CompareTo("exit") == 0)
-                {
-                    Console.WriteLine("bye!");
-                    return;
-                }
-                await OnLookupAsync(hostname);
-                Console.WriteLine();
-            } while (true);
+            Console.WriteLine($"Address Family: {address.AddressFamily}, address: {address}");
         }
-
-        public static async Task OnLookupAsync(string hostname)
-        {
-            try
-            {
-                IPHostEntry ipHost = await Dns.GetHostEntryAsync(hostname);
-
-                Console.WriteLine($"Hostname: {ipHost.HostName}");
-
-                // Aliases not populated by GetHostEntryAsync
-                //if (ipHost.Aliases.Length != 0)
-                //{
-                //    Console.WriteLine($"Aliases: {string.Join(", ", ipHost.Aliases)}");
-                //}
-
-                foreach (IPAddress address in ipHost.AddressList)
-                {
-                    Console.WriteLine($"Address Family: {address.AddressFamily}");
-                    Console.WriteLine($"Address: {address}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
     }
 }
