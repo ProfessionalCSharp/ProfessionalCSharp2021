c0e473e0301706fd00f7127c9e5340516af53638
Christian Nagel <christian@christiannagel.com>
2021-01-24 12:35:51 +0100

    System.CommandLine with utilities #43

--- 02_Libs/Networking/NetworkingSamples/Utilities/Program.cs
+++ 02_Libs/Networking/NetworkingSamples/Utilities/Program.cs
@@ -1,90 +1,89 @@
 ﻿using System;
-using System.Linq;
+using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
 using System.Net;
 
-namespace Utilities
+await BuildCommandLine()
+    .UseDefaults()
+    .Build()
+    .InvokeAsync(args);
+
+CommandLineBuilder BuildCommandLine()
 {
-    class Program
+    RootCommand rootCommand = new("Utilities");
+    Command uriCommand = new("uri", "Show the parts of the URI, e.g. www.wrox.com")
     {
-        private static Command<string>[]? s_Commands;
-
-        static void Main(string[] args)
+        new Option<string>("--uri")
         {
-            s_Commands = SetupCommands();
-
-            if (args == null || args.Length != 2 || !s_Commands.Select(c => c.Option).Contains(args[0]))
-            {
-                ShowUsage();
-                return;
-            }
-
-            s_Commands.Single(c => c.Option == args[0]).Action(args[1]);
-            Console.ReadLine();
+            IsRequired = true
         }
-
-        private static Command<string>[] SetupCommands() =>
-            new Command<string>[]
-            {
-                new Command<string>("uri", "\tShow the part of the URI, e.g. uri www.wrox.com", UriSample),
-                new Command<string>("ipaddress", "\tShow the part of the IP address, e.g. ipaddress 234.56.78.9", IPAddressSample),
-            };
-
-
-        private static void ShowUsage()
+    };
+    uriCommand.Handler = CommandHandler.Create<string>(UriSample);
+    Command ipCommand = new Command("ip", "Show the part of the IP address, e.g. ipaddress 234.56.78.9")
+    {
+        new Option<string>("--ip")
         {
-            Console.WriteLine("Usage: Utilities command\n");
-            Console.WriteLine();
-            Console.WriteLine("commands");
-            if (s_Commands == null) throw new InvalidOperationException();
-            foreach (var command in s_Commands)
-            {
-                Console.WriteLine($"{command.Option} {command.Text}");
-            }
-            Console.WriteLine();
+            IsRequired = true
         }
+    };
+    ipCommand.Handler = CommandHandler.Create<string>(ip => IPAddressSample(ip));
+    Command buildUriCommand = new("builduri", "Build an URI using the UriBuilder")
+    {
+        Handler = CommandHandler.Create(BuildUri)
+    };
 
-        public static void IPAddressSample(string ipAddressString)
-        {           
-            if (!IPAddress.TryParse(ipAddressString, out IPAddress? address))
-            {
-                Console.WriteLine($"cannot parse {ipAddressString}");
-                return;
-            }
+    rootCommand.AddCommand(uriCommand);
+    rootCommand.AddCommand(ipCommand);
+    rootCommand.AddCommand(buildUriCommand);
+    return new CommandLineBuilder(rootCommand);
+}
 
-            byte[] bytes = address.GetAddressBytes();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                Console.WriteLine($"byte {i}: {bytes[i]:X}");
-            }
-
-            Console.WriteLine($"family: {address.AddressFamily}, map to ipv6: {address.MapToIPv6()}, map to ipv4: {address.MapToIPv4()}");
-            Console.WriteLine($"IPv4 loopback address: {IPAddress.Loopback}");
-            Console.WriteLine($"IPv6 loopback address: {IPAddress.IPv6Loopback}");
-            Console.WriteLine($"IPv4 broadcast address: {IPAddress.Broadcast}");
-            Console.WriteLine($"IPv4 any address: {IPAddress.Any}");
-            Console.WriteLine($"IPv6 any address: {IPAddress.IPv6Any}");
-        }
-
-        public static void UriSample(string url)
-        {
-            var page = new Uri(url);
-            Console.WriteLine($"scheme: {page.Scheme}");
-
-            Console.WriteLine($"host: {page.Host}, type:  {page.HostNameType}, idn host: {page.IdnHost}");
-            Console.WriteLine($"port: {page.Port}");
-            Console.WriteLine($"path: {page.AbsolutePath}");
-            Console.WriteLine($"query: {page.Query}");
-            foreach (var segment in page.Segments)
-            {
-                Console.WriteLine($"segment: {segment}");
-            }
-
-            var builder = new UriBuilder();
-            builder.Host = "www.cninnovation.com";
-            builder.Port = 80;
-            builder.Path = "training/MVC";
-            Uri uri = builder.Uri;
-            Console.WriteLine(uri);
-        }
+void IPAddressSample(string ipAddressString)
+{
+    if (!IPAddress.TryParse(ipAddressString, out IPAddress? address))
+    {
+        Console.WriteLine($"cannot parse {ipAddressString}");
+        return;
     }
+
+    byte[] bytes = address.GetAddressBytes();
+    for (int i = 0; i < bytes.Length; i++)
+    {
+        Console.WriteLine($"byte {i}: {bytes[i]:X}");
+    }
+
+    Console.WriteLine($"family: {address.AddressFamily}, map to ipv6: {address.MapToIPv6()}, map to ipv4: {address.MapToIPv4()}");
+    Console.WriteLine($"IPv4 loopback address: {IPAddress.Loopback}");
+    Console.WriteLine($"IPv6 loopback address: {IPAddress.IPv6Loopback}");
+    Console.WriteLine($"IPv4 broadcast address: {IPAddress.Broadcast}");
+    Console.WriteLine($"IPv4 any address: {IPAddress.Any}");
+    Console.WriteLine($"IPv6 any address: {IPAddress.IPv6Any}");
+}
+
+void UriSample(string uri)
+{
+    Uri page = new(uri);
+    Console.WriteLine($"scheme: {page.Scheme}");
+
+    Console.WriteLine($"host: {page.Host}, type:  {page.HostNameType}, idn host: {page.IdnHost}");
+    Console.WriteLine($"port: {page.Port}");
+    Console.WriteLine($"path: {page.AbsolutePath}");
+    Console.WriteLine($"query: {page.Query}");
+    foreach (var segment in page.Segments)
+    {
+        Console.WriteLine($"segment: {segment}");
+    }
+}
+
+void BuildUri()
+{
+    UriBuilder builder = new();
+    builder.Scheme = "https";
+    builder.Host = "www.cninnovation.com";
+    builder.Port = 443;
+    builder.Path = "training/MVC";
+    Uri uri = builder.Uri;
+    Console.WriteLine(uri);
 }
