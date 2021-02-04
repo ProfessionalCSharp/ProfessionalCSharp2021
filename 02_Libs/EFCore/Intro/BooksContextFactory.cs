11f9a9caa8dc92927fbebc1a1e8d06480cec33c8
Christian Nagel <christian@christiannagel.com>
2021-02-04 15:36:47 +0100

    add migrations to ef core intro #44

---
+++ 02_Libs/EFCore/Intro/BooksContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+
+class BooksContextFactory : IDesignTimeDbContextFactory<BooksContext>
+{
+    public BooksContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<BooksContext> optionsBuilder = new();
+        string connectionString = "server=(localdb)\\mssqllocaldb;database=ProCSharpBooksSample;trusted_connection=true";
+        optionsBuilder.UseSqlServer(connectionString);
+        return new BooksContext(optionsBuilder.Options);   
+    }
+}
+
