ae0bfb3376d4f3fd9fe26cfac03166ac9a934624
Christian Nagel <christian@christiannagel.com>
2021-02-05 16:09:35 +0100

    removed not necessary checks

--- 01_CS/ObjectOrientation/OOSamples/UsingInterfaces/Person.cs
+++ 01_CS/ObjectOrientation/OOSamples/UsingInterfaces/Person.cs
@@ -4,10 +4,10 @@ public record Person(string FirstName, string LastName) : IComparable<Person>
 {
     public int CompareTo(Person? other)
     {
-        int compare = LastName?.CompareTo(other?.LastName) ?? -1;
+        int compare = LastName.CompareTo(other?.LastName);
         if (compare is 0)
         {
-            return FirstName?.CompareTo(other?.FirstName) ?? -1;
+            return FirstName.CompareTo(other?.FirstName);
         }
         return compare;
     }
