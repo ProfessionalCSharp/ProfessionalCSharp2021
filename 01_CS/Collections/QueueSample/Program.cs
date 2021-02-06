1ebfba3b912d366d4a4f735d6ee54efe965c63a4
Christian Nagel <christian@christiannagel.com>
2021-02-06 16:12:04 +0100

    fix random use - collections

--- 01_CS/Collections/QueueSample/Program.cs
+++ 01_CS/Collections/QueueSample/Program.cs
@@ -6,12 +6,13 @@ DocumentManager dm = new();
 Task processDocuments = ProcessDocuments.StartAsync(dm);
 
 // Create documents and add them to the DocumentManager
+Random random = new();
 for (int i = 0; i < 1000; i++)
 {
     var doc = new Document($"Doc {i}", "content");
     int queueSize = dm.AddDocument(doc);
     Console.WriteLine($"Added document {doc.Title}, queue size: {queueSize}");
-    await Task.Delay(new Random().Next(20));
+    await Task.Delay(random.Next(20));
 }
 Console.WriteLine($"finished adding documents");
 await processDocuments;
