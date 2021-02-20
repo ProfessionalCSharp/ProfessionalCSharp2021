ea87d5cf5482ed59ef05248144916d3ff3a21b75
Christian Nagel <christian@christiannagel.com>
2021-02-20 19:13:13 +0100

    scaffolding #44

---
+++ 02_Libs/EFCore/ScaffoldSample/MenuCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ScaffoldSample
+{
+    public partial class MenuCard
+    {
+        public MenuCard()
+        {
+            Menus = new HashSet<Menu>();
+        }
+
+        public int MenuCardId { get; set; }
+        public string Title { get; set; }
+        public Guid RestaurantId { get; set; }
+
+        public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
