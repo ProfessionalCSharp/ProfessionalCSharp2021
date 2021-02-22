d75fbfb64fdaf93a34e4f81c5c2860dd15b96c9a
Christian Nagel <christian@christiannagel.com>
2021-02-22 09:23:55 +0100

    windows updates #56

---
+++ 04_Apps/Windows/AttachedProperty/AttachedProperty/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AttachedProperty
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            MyAttachedPropertyProvider.SetMySample(button1, "sample value");
+
+            foreach (var item in GetChildren(grid1,
+                e => MyAttachedPropertyProvider.GetMySample(e) != string.Empty))
+            {
+                list1.Items.Add(
+                  $"{item.Name}: {MyAttachedPropertyProvider.GetMySample(item)}");
+            }
+        }
+
+        private IEnumerable<FrameworkElement> GetChildren(FrameworkElement element, Func<FrameworkElement, bool> pred)
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+                if (child != null && pred(child))
+                {
+                    yield return child;
+                }
+            }
+        }
+    }
+}
---
+++ 04_Apps/Windows/IntroXAML/IntroXAML/IntroXAML/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DataLib;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace IntroXAML
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            var button2 = new Button
+            {
+                Content = "created dynamically"
+            };
+            button2.Click += async (sender, e) => await new MessageDialog("button 2 clicked").ShowAsync();
+            stackPanel1.Children.Add(button2);
+
+            list1.Items.Add(new Person { FirstName = "Stephanie", LastName = "Nagel" });
+        }
+
+        private async void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            await new MessageDialog("button 1 clicked").ShowAsync();
+        }
+    }
+}
