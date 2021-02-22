127c7a9281ffe4a2d0a421ab03ce0d6e0cc73e9e
Christian Nagel <christian@christiannagel.com>
2021-02-22 09:09:07 +0100

    module initializer

--- 02_Libs/Libraries/CreateNuGet/SampleLib/Book.cs
+++ 02_Libs/Libraries/CreateNuGet/SampleLib/Book.cs
@@ -1,8 +1,19 @@
-﻿namespace SampleLib
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace SampleLib
 {
     public class Book
     {
         public string? Title { get; set; }
         public string? Publisher { get; set; }
+
+#if NET50
+        [ModuleInitializer]
+        internal static void Initializer()
+        {
+            Console.WriteLine("Module Initializer with Book");
+        }
+#endif
     }
 }
