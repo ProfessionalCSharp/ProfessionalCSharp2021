7e102eda2f589e9d515c5e61b35030d12a886b06
Christian Nagel <christian@christiannagel.com>
2021-02-27 19:27:28 +0100

    update to preview 4 #winui

--- 01_CS/Tasks/AsyncSamples/AsyncWindowsDesktopApp/AsyncWindowsDesktopApp/App.xaml.cs
+++ 01_CS/Tasks/AsyncSamples/AsyncWindowsDesktopApp/AsyncWindowsDesktopApp/App.xaml.cs
@@ -59,6 +59,6 @@ namespace AsyncWindowsDesktopApp
             // Save application state and stop any background activity
         }
 
-        private Window m_window;
+        private Window? m_window;
     }
 }
