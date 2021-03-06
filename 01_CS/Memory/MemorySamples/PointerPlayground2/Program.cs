e80e69c6c51ab6edaeb70f6f0d2accc0f158868f
Christian Nagel <christian@christiannagel.com>
2021-03-06 16:59:24 +0100

    pointer updates

--- 01_CS/Memory/MemorySamples/PointerPlayground2/Program.cs
+++ 01_CS/Memory/MemorySamples/PointerPlayground2/Program.cs
@@ -8,6 +8,7 @@ namespace PointerPlayground2
         {
             Console.WriteLine($"Size of CurrencyStruct struct is {sizeof(CurrencyStruct)}");
             CurrencyStruct amount1 = new(10, 10), amount2 = new(20, 20);
+
             CurrencyStruct* pAmount = &amount1;
             long* pDollars = &(pAmount->Dollars);
             byte* pCents = &(pAmount->Cents);
@@ -15,8 +16,11 @@ namespace PointerPlayground2
             Console.WriteLine($"Address of amount1 is 0x{(ulong)&amount1:X}");
             Console.WriteLine($"Address of amount2 is 0x{(ulong)&amount2:X}");
             Console.WriteLine($"Address of pAmount is 0x{(ulong)&pAmount:X}");
+            Console.WriteLine($"Value of pAmount is 0x{(ulong)pAmount:X}");
             Console.WriteLine($"Address of pDollars is 0x{(ulong)&pDollars:X}");
+            Console.WriteLine($"Value of pDollars is 0x{(ulong)pDollars:X}");
             Console.WriteLine($"Address of pCents is 0x{(ulong)&pCents:X}");
+            Console.WriteLine($"Value of pCents is 0x{(ulong)pCents:X}");
 
             // because Dollars are declared readonly in CurrencyStruct, you cannot change it with a variable of type CurrencyStruct
             // pAmount->Dollars = 20;
@@ -25,14 +29,15 @@ namespace PointerPlayground2
             Console.WriteLine($"amount1 contains {amount1}");
 
             --pAmount;   // this should get it to point to amount2
-            Console.WriteLine($"amount2 has address 0x{(ulong)pAmount:X} " +
-                $"and contains {*pAmount}");
+            Console.WriteLine($"pAmount contains the new address {(ulong)pAmount:X} " +
+                $"and references this value {*pAmount}");
 
             // do some clever casting to get pCents to point to cents
             // inside amount2
             CurrencyStruct* pTempCurrency = (CurrencyStruct*)pCents;
             pCents = (byte*)(--pTempCurrency);
-            Console.WriteLine("Address of pCents is now 0x{0:X}", (ulong)&pCents);
+            Console.WriteLine($"Value of pCents is now 0x{(ulong)pCents:X}");
+            Console.WriteLine($"The value where pCents points to: {*pCents}");
 
             Console.WriteLine("\nNow with classes");
             // now try it out with classes
