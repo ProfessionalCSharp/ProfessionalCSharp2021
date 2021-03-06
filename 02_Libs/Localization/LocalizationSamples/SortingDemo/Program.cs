b1e2a1448ef9b9e49af5331065303a3e8fe16da0
Christian Nagel <christian@christiannagel.com>
2021-03-06 20:04:57 +0100

    sorting demo #50

--- 02_Libs/Localization/LocalizationSamples/SortingDemo/Program.cs
+++ 02_Libs/Localization/LocalizationSamples/SortingDemo/Program.cs
@@ -3,33 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace SortingDemo
+string[] countries = { "Österreich", "België", "България", "Hrvatska", "Česko", "Danmark", "Eesti", "Suomi", "France",
+    "Deutschland", "Ελλάδα", "Magyarország", "Ireland", "Italia", "Latvija", "Lietuva", "Lëtzebuerg", "Malta", "Nederland",
+    "Polska", "Portugal", "România", "Slovensko", "Slovenija", "España", "Sverige" };
+
+CultureInfo.CurrentCulture = new CultureInfo("fi-FI");
+
+Array.Sort(countries);
+DisplayNames("Sorted using the Finnish culture", countries);
+
+// sort using the invariant culture
+
+Array.Sort(countries, Comparer.DefaultInvariant);
+DisplayNames("Sorted using the invariant culture", countries);
+
+void DisplayNames(string title, IEnumerable<string> names)
 {
-    class Program
-    {
-        static void Main()
-        {
-            string[] names = {"Alabama", "Texas", "Washington",
-                              "Virginia", "Wisconsin", "Wyoming",
-                              "Kentucky", "Missouri", "Utah", "Hawaii",
-                              "Kansas", "Louisiana", "Alaska", "Arizona"};
-
-            CultureInfo.CurrentCulture = new CultureInfo("fi-FI");
-
-            Array.Sort(names);
-            DisplayNames("Sorted using the Finnish culture", names);
-
-            // sort using the invariant culture
-
-            Array.Sort(names, Comparer.DefaultInvariant);
-            DisplayNames("Sorted using the invariant culture", names);
-        }
-
-        public static void DisplayNames(string title, IEnumerable<string> names)
-        {
-            Console.WriteLine(title);
-            Console.WriteLine(string.Join("-", names));
-            Console.WriteLine();
-        }
-    }
+    Console.WriteLine(title);
+    Console.WriteLine(string.Join("-", names));
+    Console.WriteLine();
 }
