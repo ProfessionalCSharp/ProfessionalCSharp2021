b9fd81bf1bbdf4871cbc1530168df1edf31dd065
Christian Nagel <christian@christiannagel.com>
2021-03-16 21:46:24 +0100

    asp.net core with WebHost #51

---
+++ 03_Web/ASPNETCore/SimpleHost/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+WebHost.Start(async context =>
+{
+    await context.Response.WriteAsync("<h1>A Simple Host!</h1>");
+}).WaitForShutdown();
