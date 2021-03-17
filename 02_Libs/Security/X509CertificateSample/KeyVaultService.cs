7b4a496778724e85abfcc9721da3eabebe73ac49
Christian Nagel <christian@christiannagel.com>
2021-03-17 09:03:54 +0100

    access certificate from key vault #46

---
+++ 02_Libs/Security/X509CertificateSample/KeyVaultService.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Certificates;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+
+class KeyVaultService
+{
+    private readonly string _vaultUri;
+    private readonly VisualStudioCredential _credential = new();
+    public KeyVaultService(IConfiguration configuration)
+    {
+        _vaultUri = configuration["VaultUri"];
+    }
+
+    public async Task<X509Certificate2> GetCertificateAsync(string name)
+    {
+        //using var listener = new AzureEventSourceListener((eventArgs, text) => AzureEventSourceHandler(eventArgs, text), EventLevel.Informational);
+
+        //static void AzureEventSourceHandler(EventWrittenEventArgs eventArgs, string text)
+        //{
+        //    Console.WriteLine(text);
+        //    if (text.Contains("Credential") && text.Contains("succeeded"))
+        //    {
+        //        Console.WriteLine(text);
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        CertificateClient certClient = new(new Uri(_vaultUri), _credential);
+        Response<KeyVaultCertificateWithPolicy> response = await certClient.GetCertificateAsync("AliceCert");
+        Uri secretId = response.Value.SecretId;
+        string secretName = secretId.Segments[2].Trim('/');
+        string version = secretId.Segments[3].TrimEnd('/');
+
+        // https://stackoverflow.com/questions/37033073/how-can-i-create-an-x509certificate2-object-from-an-azure-key-vault-keybundle
+        
+        byte[] x509cer = response.Value.Cer;
+        SecretClient secretClient = new(new Uri(_vaultUri), _credential);
+        Response<KeyVaultSecret> responseSecret = await secretClient.GetSecretAsync(secretName, version);
+        KeyVaultSecret secret = responseSecret.Value;
+        byte[] privateKeyBytes = Convert.FromBase64String(secret.Value);
+        X509Certificate2 cert = new(privateKeyBytes);
+        return cert;
+    }
+}
+
---
+++ 02_Libs/Security/X509CertificateSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+using var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<KeyVaultService>();
+    }).Build();
+
+var service = host.Services.GetRequiredService<KeyVaultService>();
+using var certificate = await service.GetCertificateAsync("AliceCert");
+
+ShowCertificate(certificate);
+
+
+void ShowCertificate(X509Certificate2 certificate)
+{
+    Console.WriteLine($"Subject: {certificate.Subject}");
+    Console.WriteLine($"Not before: {certificate.NotBefore:D}");
+    Console.WriteLine($"Not after: {certificate.NotAfter:D}");
+    Console.WriteLine($"Has private key: {certificate.HasPrivateKey}");
+    Console.WriteLine($"Key algorithm: {certificate.PublicKey.Key.KeyExchangeAlgorithm}");
+    Console.WriteLine($"Key size: {certificate.PublicKey.Key.KeySize}");
+}
