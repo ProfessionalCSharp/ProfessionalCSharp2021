4997de5908330ba4deb1411a3c17ec965010d187
Christian Nagel <christian@christiannagel.com>
2021-03-18 21:59:03 +0100

    identity sample cleanup #46

--- 02_Libs/Security/IdentitySample/Runner.cs
+++ 02_Libs/Security/IdentitySample/Runner.cs
@@ -5,6 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Id = Microsoft.Identity.Client;
 
+internal static class IdentityLogLevelExtensions
+{
+    public static LogLevel ToLogLevel(this Id.LogLevel logLevel)
+        => logLevel switch
+        {
+            Id.LogLevel.Error => LogLevel.Error,
+            Id.LogLevel.Warning => LogLevel.Warning,
+            Id.LogLevel.Info => LogLevel.Information,
+            Id.LogLevel.Verbose => LogLevel.Trace,
+            _ => throw new InvalidOperationException("unexpected log level")
+        };
+}
+
 class Runner
 {
     private readonly string _clientId;
@@ -19,37 +32,22 @@ class Runner
         _logger = logger;
     }
 
-    private void LogCallback(Id.LogLevel level, string message, bool containsPii)
-    {
-        LogLevel GetDotnetLogLevel(Id.LogLevel logLevel) =>
-            logLevel switch
-            {
-                Id.LogLevel.Error => LogLevel.Error,
-                Id.LogLevel.Warning => LogLevel.Warning,
-                Id.LogLevel.Info => LogLevel.Information,
-                Id.LogLevel.Verbose => LogLevel.Trace,
-                _ => throw new InvalidOperationException("unexpected log level")
-            };
-
-        LogLevel logLevel = GetDotnetLogLevel(level);
-
-        
-        _logger.Log(logLevel, message);
-    }
-
     public void Init()
     {
-       _clientApp = Id.PublicClientApplicationBuilder
-            .Create(_clientId)
-            .WithLogging(LogCallback, logLevel: Id.LogLevel.Verbose, enableDefaultPlatformLogging: true)
-            .WithAuthority(Id.AzureCloudInstance.AzurePublic, _tenantId)
-            .WithRedirectUri("http://localhost")
-            .Build();
+        void LogCallback(Id.LogLevel level, string message, bool containsPii)
+            => _logger.Log(level.ToLogLevel(), message);      
+
+        _clientApp = Id.PublicClientApplicationBuilder
+             .Create(_clientId)
+             .WithLogging(LogCallback, logLevel: Id.LogLevel.Verbose)
+             .WithAuthority(Id.AzureCloudInstance.AzurePublic, _tenantId)
+             .WithRedirectUri("http://localhost")
+             .Build();
     }
 
     public async Task LoginAsync()
     {
-        if (_clientApp is null) throw new InvalidOperationException("Invoke Init first");
+        if (_clientApp is null) throw new InvalidOperationException("Invoke Init before calling this method");
 
         try
         {
@@ -57,32 +55,35 @@ class Runner
             var accounts = await _clientApp.GetAccountsAsync();
             var firstAccount = accounts.FirstOrDefault();
             if (firstAccount is not null)
-            {
-                Id.AuthenticationResult result = await _clientApp.AcquireTokenSilent(scopes, firstAccount).ExecuteAsync();
+            {                   
+                Id.AuthenticationResult result = await _clientApp.AcquireTokenSilent(scopes, firstAccount)
+                    .ExecuteAsync();
                 ShowAuthenticationResult(result);
             }
             else
             {
-                Id.AuthenticationResult result = await _clientApp.AcquireTokenInteractive(scopes).ExecuteAsync();
+                Id.AuthenticationResult result = await _clientApp.AcquireTokenInteractive(scopes)
+                    .ExecuteAsync();
                 ShowAuthenticationResult(result);
             }
-
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, ex.Message);
+            throw;
         }
     }
 
     private void ShowAuthenticationResult(Id.AuthenticationResult result)
     {
-        Console.WriteLine($"token: {result.AccessToken}");
-        Console.WriteLine(result.Account.Username);
-        Console.WriteLine(result.Account.Environment);
-        Console.WriteLine(result.Account.HomeAccountId);
+        Console.WriteLine($"Id token: {result.IdToken[..20]}");
+        Console.WriteLine($"Access token: {result.AccessToken[..20]}");
+        Console.WriteLine($"Username: {result.Account.Username}");
+        Console.WriteLine($"Environment: {result.Account.Environment}");
+        Console.WriteLine($"Account Id: {result.Account.HomeAccountId}");
         foreach (var scope in result.Scopes)
         {
-            Console.WriteLine(scope);
+            Console.WriteLine($"scope: {scope}");
         }
     }
 }
