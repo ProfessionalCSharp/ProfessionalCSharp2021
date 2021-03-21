cb77b17dd54ff1c4fdf5d2e9e3e7fc902804e89f
Christian Nagel <christian@christiannagel.com>
2021-03-21 10:07:19 +0100

    trace logging and async extension with simple host #51

--- 03_Web/ASPNETCore/SimpleHost/Program.cs
+++ 03_Web/ASPNETCore/SimpleHost/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 
-WebHost.Start(async context =>
+await WebHost.Start(async context =>
 {
     await context.Response.WriteAsync("<h1>A Simple Host!</h1>");
-}).WaitForShutdown();
+}).WaitForShutdownAsync();
