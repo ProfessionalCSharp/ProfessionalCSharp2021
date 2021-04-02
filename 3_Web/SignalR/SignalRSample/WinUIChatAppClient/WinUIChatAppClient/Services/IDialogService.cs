c0102510c01a0f9aed4856d92e7221d09cda6a98
Christian Nagel <christian@christiannagel.com>
2021-04-02 14:51:14 +0200

    remove old samples #53

--- 3_Web/SignalR/SignalRSample/WinUIChatAppClient/WinUIChatAppClient/Services/IDialogService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ChatClient.Services
-{
-    public interface IDialogService
-    {
-        Task ShowMessageAsync(string message);
-    }
-}
--- 3_Web/SignalR/SignalRSample/WinUIChatAppClient/WinUIChatAppClient/Services/UrlService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatClient.Services
-{
-    public class UrlService
-    {
-        private const string BaseUri = "http://localhost:3509";
-        public string ChatAddress => $"{BaseUri}/chat";
-        public string GroupAddress => $"{BaseUri}/groupchat";
-    }
-}
