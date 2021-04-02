ae4b4490e1215bcd0b0fcf255b7058c947eeeaf2
Christian Nagel <christian@christiannagel.com>
2021-04-02 16:56:46 +0200

    signalr streaming #53

---
+++ 3_Web/SignalR/StreamingClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+Console.WriteLine($"Wait for service - press return to start");
+Console.ReadLine();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:5001/stream")
+    .Build();
+
+await connection.StartAsync();
+
+CancellationTokenSource cts = new(10000);
+
+try
+{
+    await foreach (var data in connection.StreamAsync<SensorData>("GetSensorData").WithCancellation(cts.Token))
+    {
+        Console.WriteLine(data);
+    }
+}
+catch (OperationCanceledException)
+{
+    Console.WriteLine("Canceled!");
+}
+
+await connection.StopAsync();
+
+Console.WriteLine("Completed");
+
+
+public record SensorData(int Val1, int Val2, DateTime TimeStamp);
