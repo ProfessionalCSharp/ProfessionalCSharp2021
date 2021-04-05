7ccd6da1da998c9642fea4a7713198ba47ed2d2d
Christian Nagel <christian@christiannagel.com>
2021-04-05 13:22:59 +0200

    extra sync objects

--- 2_Libs/Parallel/SynchronizationSamples/ThreadingIssues/TaskWithDeadlock.cs
+++ 2_Libs/Parallel/SynchronizationSamples/ThreadingIssues/TaskWithDeadlock.cs
@@ -9,6 +9,8 @@ namespace ThreadingIssues
 
         private readonly StateObject _s1;
         private readonly StateObject _s2;
+        private readonly object _sync1 = new object();
+        private readonly object _sync2 = new object();
         
         public void Deadlock1()
         {
@@ -16,10 +18,10 @@ namespace ThreadingIssues
             while (true)
             {
                 Console.WriteLine("1 - waiting for s1");
-                lock (_s1)
+                lock (_sync1)
                 {
                     Console.WriteLine("1 - s1 locked, waiting for s2");
-                    lock (_s2)
+                    lock (_sync2)
                     {
                         Console.WriteLine("1 - s1 and s2 locked, now go on...");
                         _s1.ChangeState(i);
@@ -37,10 +39,10 @@ namespace ThreadingIssues
             {
                 Console.WriteLine("2 - waiting for s2");
 
-                lock (_s2)
+                lock (_sync2)
                 {
                     Console.WriteLine("2 - s2 locked, waiting for s1");
-                    lock (_s1)
+                    lock (_sync1)
                     {
                         Console.WriteLine("2 - s1 and s2 locked, now go on...");
                         _s1.ChangeState(i);
