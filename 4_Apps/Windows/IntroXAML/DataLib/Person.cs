776bab6319ad96806ab9a5ac05815afa8930e496
Christian Nagel <christian@christiannagel.com>
2021-04-18 19:46:30 +0200

    intro xaml #64

--- 4_Apps/Windows/IntroXAML/DataLib/Person.cs
+++ 4_Apps/Windows/IntroXAML/DataLib/Person.cs
@@ -2,6 +2,9 @@
 {
     public class Person
     {
+        public Person(string firstName, string lastName) =>
+            (FirstName, LastName) = (firstName, lastName);
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public override string ToString() => $"{FirstName} {LastName}";
--- 4_Apps/Windows/IntroXAML/IntroXAML/IntroXAML/MainWindow.xaml.cs
+++ 4_Apps/Windows/IntroXAML/IntroXAML/IntroXAML/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using DataLib;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -28,14 +17,14 @@ namespace IntroXAML
         public MainWindow()
         {
             this.InitializeComponent();
-            var button2 = new Button
+            Button button2 = new()
             {
                 Content = "created dynamically"
             };
             button2.Click += async (sender, e) => await new MessageDialog("button 2 clicked").ShowAsync();
             stackPanel1.Children.Add(button2);
 
-            list1.Items.Add(new Person { FirstName = "Stephanie", LastName = "Nagel" });
+            list1.Items.Add(new Person("Stephanie", "Nagel"));
         }
 
         private async void OnButtonClick(object sender, RoutedEventArgs e)
