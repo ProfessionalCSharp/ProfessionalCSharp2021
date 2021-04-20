75b360b6a6193b18381f6ba6cb49b0e715c82958
Christian Nagel <christian@christiannagel.com>
2021-04-20 11:24:38 +0200

    intro xaml #56

--- 4_Apps/Windows/IntroXAML/IntroXAML/IntroXAML/IntroXAML/MainWindow.xaml.cs
+++ 4_Apps/Windows/IntroXAML/IntroXAML/IntroXAML/IntroXAML/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using Windows.UI.Popups;
+using WinRT;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,8 +26,21 @@ namespace IntroXAML
             };
             button2.Click += async (sender, e) =>
             {
+                // TODO: change to (with version 0.8):
+                // await new MessageDialog("button 2 clicked").ShowAsync();
                 MessageDialog dlg = new("button 2 clicked");
-                await dlg.ShowAsync();
+
+                IInitializeWithWindow withWindow = dlg.As<IInitializeWithWindow>();
+                var handle = this.As<IWindowNative>().WindowHandle;
+                try
+                {
+                    withWindow.Initialize(handle);
+                    await dlg.ShowAsync();
+                }
+                catch (System.Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
             };
             stackPanel1.Children.Add(button2);
 
@@ -33,7 +49,38 @@ namespace IntroXAML
 
         private async void OnButtonClick(object sender, RoutedEventArgs e)
         {
-            await new MessageDialog("button 1 clicked").ShowAsync();
+            // TODO: change to (with version 0.8):
+            // await new MessageDialog("button 1 clicked").ShowAsync();
+
+            MessageDialog dlg = new("button 1 clicked");
+
+            IInitializeWithWindow withWindow = dlg.As<IInitializeWithWindow>();
+            var handle = this.As<IWindowNative>().WindowHandle;
+            try
+            {
+                withWindow.Initialize(handle);
+                await dlg.ShowAsync();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
         }
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+    internal interface IInitializeWithWindow
+    {
+        void Initialize(IntPtr hwnd);
+    }
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+    internal interface IWindowNative
+    {
+        IntPtr WindowHandle { get; }
     }
 }
