670ec672ac00d337548ee7b2a2cb2a6eda2b56f0
Christian Nagel <christian@christiannagel.com>
2021-04-20 17:07:45 +0200

    cleanup #56

--- 4_Apps/Windows/DataBindingSamples/DataBindingSamples/App.xaml.cs
+++ 4_Apps/Windows/DataBindingSamples/DataBindingSamples/App.xaml.cs
@@ -1,20 +1,4 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,10 +14,7 @@ namespace DataBindingSamples
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-        }
+        public App() => this.InitializeComponent();
 
         /// <summary>
         /// Invoked when the application is launched normally by the end user.  Other entry points
--- 4_Apps/Windows/DataBindingSamples/DataBindingSamples/MainWindow.xaml.cs
+++ 4_Apps/Windows/DataBindingSamples/DataBindingSamples/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using DataBindingSamples.Models;
 using DataBindingSamples.Services;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,21 +15,15 @@ namespace DataBindingSamples
     public sealed partial class MainWindow : Window
     {
         private BooksService _booksService = new();
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+        public MainWindow() => InitializeComponent();
 
         public ObservableCollection<Book> Books => _booksService.Books;
 
-        public void OnRefreshBooks()
-        {
-            _booksService.RefreshBooks();
-        }
+        public void OnRefreshBooks() => _booksService.RefreshBooks();
 
         public void OnAddBook() =>
           _booksService.AddBook(new Book(GetNextBookId(),
-            $"Professional C# {GetNextBookId() + 3}", "Wrox Press"));
+            $"Professional C# {GetNextBookId() + 4}", "Wrox Press"));
 
         private int GetNextBookId() => Books.Select(b => b.BookId).Max() + 1;
     }
