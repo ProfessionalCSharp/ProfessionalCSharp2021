c4d9dc5e53d38624d4bc76df425d8397b6337aa4
Christian Nagel <christian@christiannagel.com>
2021-04-22 13:27:27 +0200

    minor updates and nullability - blazor server #11

--- 3_Web/Blazor/Blazor.ServerSample/Data/WeatherForecast.cs
+++ 3_Web/Blazor/Blazor.ServerSample/Data/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace Blazor.ServerSample.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
--- 3_Web/Blazor/Blazor.ServerSample/Startup.cs
+++ 3_Web/Blazor/Blazor.ServerSample/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Blazor.ServerSample.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Blazor.ServerSample.Data;
 
 namespace Blazor.ServerSample
 {
