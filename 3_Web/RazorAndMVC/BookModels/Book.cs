e89249dab1c4dcfe7da589938411d78b8ad0cf71
Christian Nagel <christian@christiannagel.com>
2021-04-24 23:13:58 +0200

    EF Core model #52

---
+++ 3_Web/RazorAndMVC/BookModels/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace BookModels
+{
+    public record Book(
+        [property: MaxLength(50)]
+        [property: DisplayName("Title")]
+        string Title,
+        [property:MaxLength(50)]
+        [property:DisplayName("Publisher")]
+        string Publisher,
+        int BookId = 0);
+}
