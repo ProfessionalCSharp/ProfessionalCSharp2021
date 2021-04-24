c79c1767d50b9b052133c9ee0c5ccd80c94a072f
Christian Nagel <christian@christiannagel.com>
2021-04-24 20:45:14 +0200

    custom tag helpers #52

---
+++ 3_Web/RazorAndMVC/CustomTagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CustomTagHelpers
+{
+    [HtmlTargetElement("markdown", TagStructure = TagStructure.NormalOrSelfClosing)]
+    [HtmlTargetElement(Attributes = "markdownfile")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        private readonly IHostEnvironment _env;
+        public MarkdownTagHelper(IHostEnvironment env) => _env = env;
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            string markdown;
+            if (MarkdownFile is not null)
+            {
+                string filename = Path.Combine(_env.ContentRootPath, MarkdownFile);
+                markdown = File.ReadAllText(filename);
+            }
+            else
+            {
+                markdown = (await output.GetChildContentAsync()).GetContent();
+            }
+            output.Content.SetHtmlContent(Markdown.ToHtml(markdown));
+        }
+
+        [HtmlAttributeName("markdownfile")]
+        public string? MarkdownFile { get; set; }
+    }
+}
