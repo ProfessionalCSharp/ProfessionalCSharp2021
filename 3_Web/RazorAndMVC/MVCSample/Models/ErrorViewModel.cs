e0195e36c0f5222daa4d53265a4cd6df75c0fe8b
Christian Nagel <christian@christiannagel.com>
2021-04-25 16:26:50 +0200

    MVC sample #52

---
+++ 3_Web/RazorAndMVC/MVCSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
