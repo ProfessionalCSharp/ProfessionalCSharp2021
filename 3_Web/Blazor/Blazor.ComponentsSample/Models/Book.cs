78f0538095c7dba474041f2a832f0a3474fc79c8
Christian Nagel <christian@christiannagel.com>
2021-04-29 22:42:47 +0200

    blazor components #11

---
+++ 3_Web/Blazor/Blazor.ComponentsSample/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blazor.ComponentsSample.Shared
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
--- 3_Web/Blazor/Blazor.ComponentsSample/Program.cs
+++ 3_Web/Blazor/Blazor.ComponentsSample/Program.cs
@@ -1,14 +1,11 @@
+using Blazor.ComponentsSample.Client.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
-namespace Blazor.ComponentsSample.Client
+namespace Blazor.ComponentsSample
 {
     public class Program
     {
@@ -18,6 +15,7 @@ namespace Blazor.ComponentsSample.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<CounterService>();
 
             await builder.Build().RunAsync();
         }
