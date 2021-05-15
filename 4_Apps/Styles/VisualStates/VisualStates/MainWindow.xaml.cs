14ec53af8b0ad6bf00daea26c8d43e4b1d4962f2
Christian Nagel <christian@christiannagel.com>
2021-05-15 14:29:56 +0200

    cleanup

--- 4_Apps/Styles/VisualStates/VisualStates/MainWindow.xaml.cs
+++ 4_Apps/Styles/VisualStates/VisualStates/MainWindow.xaml.cs
@@ -12,14 +12,10 @@ namespace VisualStates
     {
         public MainWindow() => InitializeComponent();
 
-        private void OnEnable(object sender, RoutedEventArgs e)
-        {
+        private void OnEnable(object sender, RoutedEventArgs e) =>
             VisualStateManager.GoToState(page1, "Enabled", useTransitions: false);
-        }
 
-        private void OnDisable(object sender, RoutedEventArgs e)
-        {
+        private void OnDisable(object sender, RoutedEventArgs e) =>
             VisualStateManager.GoToState(page1, "Disabled", useTransitions: false);
-        }
     }
 }
