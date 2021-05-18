cf3e16f7b1f35880fc1a2f1c93703d5ecc586f3f
Christian Nagel <christian@christiannagel.com>
2021-05-18 20:11:37 +0200

    web app and winui analytics

--- 2_Libs/LoggingAndMetrics/WindowsAppAnalytics/WindowsAppAnalytics/WindowsAppAnalytics/MainWindow.xaml.cs
+++ 2_Libs/LoggingAndMetrics/WindowsAppAnalytics/WindowsAppAnalytics/WindowsAppAnalytics/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿// using Microsoft.AppCenter.Analytics;
+﻿using Microsoft.AppCenter.Analytics;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -16,20 +17,20 @@ namespace WindowsAppAnalytics
         {
             Title = "WinUI App with AppCenter Analytics";
             this.InitializeComponent();
-            // Analytics.TrackEvent(EventNames.PageNavigation, new Dictionary<string, string> { ["Page"] = nameof(MainWindow) });
+            Analytics.TrackEvent(EventNames.PageNavigation, new Dictionary<string, string> { ["Page"] = nameof(MainWindow) });
         }
 
         private void OnButtonClick(object sender, RoutedEventArgs e)
         {
-            // Analytics.TrackEvent(EventNames.ButtonClicked, new Dictionary<string, string> { ["State"] = textState.Text });
+            Analytics.TrackEvent(EventNames.ButtonClicked, new Dictionary<string, string> { ["State"] = textState.Text });
         }
 
-        private void OnAnalyticsChanged(object sender, RoutedEventArgs e)
+        private async void OnAnalyticsChanged(object sender, RoutedEventArgs e)
         {
             if (sender is CheckBox checkbox)
             {
                 bool isChecked = checkbox?.IsChecked ?? true;
-                // await Analytics.SetEnabledAsync(isChecked);
+                await Analytics.SetEnabledAsync(isChecked);
             }
         }
     }
