611c5f7ad6d40413edd31ce9f615c907a2a0c7b3
Christian Nagel <christian@christiannagel.com>
2021-06-30 15:25:36 +0200

    remove not used project

--- 2_Libs/EFCore/OwnedEntities/Person.cs
+++
@@ -1,6 +0,0 @@
-﻿public record Location(string Country, string City);
-
-public record Address(string? LineOne = default, string? LineTwo = default, Location? Location = default);
-
-public record Person(string FirstName, string LastName, Address? PrivateAddress = default, Address? CompanyAddress = default);
-
