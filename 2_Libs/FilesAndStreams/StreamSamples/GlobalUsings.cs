d7dc9f64b4f1541393717defeb01337efaf3e5e6
Christian Nagel <christian@christiannagel.com>
2021-09-04 21:35:22 +0200

    C# 10 - files and streams

---
+++ 2_Libs/FilesAndStreams/StreamSamples/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System.Text;
+global using System.CommandLine;
+global using System.CommandLine.Builder;
+global using System.CommandLine.Invocation;
+global using System.CommandLine.Parsing;
+global using System.Globalization;
