cbd5db666e9b0f02680d154bb73320ace200ba40
Christian Nagel <christian@christiannagel.com>
2021-10-28 12:19:57 +0200

    .net 6: temporal tables with ef core 6

---
+++ 2_Libs/EFCore/TemporalTableSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var connectionString = context.Configuration.GetConnectionString("BooksConnection");
+        services.AddDbContext<BooksContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+        services.AddScoped<Runner>();
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var runner = scope.ServiceProvider.GetRequiredService<Runner>();
+
+await runner.CreateTheDatabaseAsync();
+await runner.AddBookAsync("Professional C# and .NET", "Wrox Press");
+await runner.AddBooksAsync();
+await runner.ReadBooksAsync();
+await runner.QueryBooksAsync();
+await runner.UpdateBookAsync();
+await runner.DeleteBooksAsync();
+await runner.DeleteDatabaseAsync();
