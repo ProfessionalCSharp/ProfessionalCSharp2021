2f6099542979f6718d1b454a54b93e15cea2f7a5
Christian Nagel <christian@christiannagel.com>
2021-11-01 10:54:09 +0100

    .NET 6 RC2 updates for generated web app

--- 1_CS/HelloWorld/WebApp/Program.cs
+++ 1_CS/HelloWorld/WebApp/Program.cs
@@ -1,4 +1,3 @@
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,11 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
