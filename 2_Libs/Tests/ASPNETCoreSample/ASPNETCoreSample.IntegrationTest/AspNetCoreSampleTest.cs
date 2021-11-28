f63d4dc7b461ee9f0259732485c7c9474ae697e1
Christian Nagel <christian@christiannagel.com>
2021-11-28 11:24:29 +0100

    asp.net core integration test simple .NET 6 updates #79

--- 2_Libs/Tests/ASPNETCoreSample/ASPNETCoreSample.IntegrationTest/AspNetCoreSampleTest.cs
+++ 2_Libs/Tests/ASPNETCoreSample/ASPNETCoreSample.IntegrationTest/AspNetCoreSampleTest.cs
@@ -1,29 +1,27 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace ASPNETCoreSample.IntegrationTest
+namespace ASPNETCoreSample.IntegrationTest;
+
+public class ASPNETCoreSampleTest
+    : IClassFixture<WebApplicationFactory<ASPNETCoreSample.Startup>>
 {
-    public class ASPNETCoreSampleTest
-        : IClassFixture<WebApplicationFactory<ASPNETCoreSample.Startup>>
+    private readonly WebApplicationFactory<ASPNETCoreSample.Startup> _factory;
+
+    public ASPNETCoreSampleTest(WebApplicationFactory<ASPNETCoreSample.Startup> factory)
+        => _factory = factory;
+
+    [Fact]
+    public async Task ReturnHelloWorld()
     {
-        private readonly WebApplicationFactory<ASPNETCoreSample.Startup> _factory;
+        // arrange
+        var client = _factory.CreateClient();
+        // act
+        var response = await client.GetAsync("/");
 
-        public ASPNETCoreSampleTest(WebApplicationFactory<ASPNETCoreSample.Startup> factory)
-            => _factory = factory;
-
-        [Fact]
-        public async Task ReturnHelloWorld()
-        {          
-            // arrange
-            var client = _factory.CreateClient();
-            // act
-            var response = await client.GetAsync("/");
-
-            // assert
-            response.EnsureSuccessStatusCode();
-            string responseString = await response.Content.ReadAsStringAsync();
-            Assert.Equal("Hello World!", responseString);
-        }
+        // assert
+        response.EnsureSuccessStatusCode();
+        string responseString = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello World!", responseString);
     }
 }
