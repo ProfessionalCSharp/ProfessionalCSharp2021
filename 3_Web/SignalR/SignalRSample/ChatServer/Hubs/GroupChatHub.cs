f2e20aedbcfba5aa699a1bd633aeb70e12619562
Christian Nagel <christian@christiannagel.com>
2021-12-02 08:24:10 +0100

    signalr chatserver .net 6 updates #79

--- 3_Web/SignalR/SignalRSample/ChatServer/Hubs/GroupChatHub.cs
+++ 3_Web/SignalR/SignalRSample/ChatServer/Hubs/GroupChatHub.cs
@@ -1,28 +1,26 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Threading.Tasks;
+
 using System.Web;
 
-namespace ChatServer.Hubs
+namespace ChatServer.Hubs;
+
+public interface IGroupClient
 {
-    public interface IGroupClient
-    {
-        Task MessageToGroup(string groupName, string name, string message);
-    }
+    Task MessageToGroup(string groupName, string name, string message);
+}
 
-    public class GroupChatHub : Hub<IGroupClient>
-    {
-        public Task AddGroup(string groupName)
-            => Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+public class GroupChatHub : Hub<IGroupClient>
+{
+    public Task AddGroup(string groupName)
+        => Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-        public Task LeaveGroup(string groupName)
-            => Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+    public Task LeaveGroup(string groupName)
+        => Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
 
-        public Task Send(string group, string name, string message) =>
-            Clients.Group(group).MessageToGroup(group, HttpUtility.HtmlEncode(name), HttpUtility.HtmlEncode(message));
+    public Task Send(string group, string name, string message) =>
+        Clients.Group(group).MessageToGroup(group, HttpUtility.HtmlEncode(name), HttpUtility.HtmlEncode(message));
 
-        public override Task OnConnectedAsync() => base.OnConnectedAsync();
+    public override Task OnConnectedAsync() => base.OnConnectedAsync();
 
-        public override Task OnDisconnectedAsync(Exception? exception) => base.OnDisconnectedAsync(exception);
-    }
+    public override Task OnDisconnectedAsync(Exception? exception) => base.OnDisconnectedAsync(exception);
 }
