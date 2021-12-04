4698be5280faf911d1884e14121c8688bdfa303c
Christian Nagel <christian@christiannagel.com>
2021-12-04 20:51:31 +0100

    web app - .net 6 update #79

--- 3_Web/ASPNETCore/WebSampleApp/Middleware/HeaderMiddleware.cs
+++ 3_Web/ASPNETCore/WebSampleApp/Middleware/HeaderMiddleware.cs
@@ -1,27 +1,22 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
+﻿namespace WebSampleApp.Middleware;
 
-namespace WebSampleApp.Middleware
+// You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+public class HeaderMiddleware
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
-    public class HeaderMiddleware
+    private readonly RequestDelegate _next;
+
+    public HeaderMiddleware(RequestDelegate next) => _next = next;
+
+    public Task Invoke(HttpContext httpContext)
     {
-        private readonly RequestDelegate _next;
-
-        public HeaderMiddleware(RequestDelegate next) => _next = next;
-
-        public Task Invoke(HttpContext httpContext)
-        {
-            httpContext.Response.Headers.Add("CustomHeader2", "custom header value");
-            return _next(httpContext);
-        }
+        httpContext.Response.Headers.Add("CustomHeader2", "custom header value");
+        return _next(httpContext);
     }
+}
 
-    // Extension method used to add the middleware to the HTTP request pipeline.
-    public static class HeaderMiddlewareExtensions
-    {
-        public static IApplicationBuilder UseHeaderMiddleware(this IApplicationBuilder builder) 
-            => builder.UseMiddleware<HeaderMiddleware>();
-    }
+// Extension method used to add the middleware to the HTTP request pipeline.
+public static class HeaderMiddlewareExtensions
+{
+    public static IApplicationBuilder UseHeaderMiddleware(this IApplicationBuilder builder)
+        => builder.UseMiddleware<HeaderMiddleware>();
 }
--- 3_Web/ASPNETCore/WebSampleApp/Services/SessionSample.cs
+++ 3_Web/ASPNETCore/WebSampleApp/Services/SessionSample.cs
@@ -1,31 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Globalization;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
-namespace WebSampleApp.Services
+namespace WebSampleApp.Services;
+
+public class SessionSample
 {
-    public class SessionSample
-    {
-        private const string SessionVisits = nameof(SessionVisits);
-        private const string SessionTimeCreated = nameof(SessionTimeCreated);
+    private const string SessionVisits = nameof(SessionVisits);
+    private const string SessionTimeCreated = nameof(SessionTimeCreated);
 
-        public  async Task SessionAsync(HttpContext context)
+    public async Task SessionAsync(HttpContext context)
+    {
+        int visits = context.Session.GetInt32(SessionVisits) ?? 0;
+        string timeCreated = context.Session.GetString(SessionTimeCreated) ??
+          string.Empty;
+        if (string.IsNullOrEmpty(timeCreated))
         {
-            int visits = context.Session.GetInt32(SessionVisits) ?? 0;
-            string timeCreated = context.Session.GetString(SessionTimeCreated) ??
-              string.Empty;
-            if (string.IsNullOrEmpty(timeCreated))
-            {
-                timeCreated = DateTime.Now.ToString("t", CultureInfo.InvariantCulture);
-                context.Session.SetString(SessionTimeCreated, timeCreated);
-            }
-            DateTime timeCreated2 = DateTime.Parse(timeCreated);
-            context.Session.SetInt32(SessionVisits, ++visits);
-            await context.Response.WriteAsync(
-              $"Number of visits within this session: {visits} " +
-              $"that was created at {timeCreated2:T}; " +
-              $"current time: {DateTime.Now:T}");
+            timeCreated = DateTime.Now.ToString("t", CultureInfo.InvariantCulture);
+            context.Session.SetString(SessionTimeCreated, timeCreated);
         }
+        DateTime timeCreated2 = DateTime.Parse(timeCreated);
+        context.Session.SetInt32(SessionVisits, ++visits);
+        await context.Response.WriteAsync(
+          $"Number of visits within this session: {visits} " +
+          $"that was created at {timeCreated2:T}; " +
+          $"current time: {DateTime.Now:T}");
     }
 }
