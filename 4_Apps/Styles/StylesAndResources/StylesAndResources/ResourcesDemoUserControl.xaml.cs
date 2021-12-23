86773dae19882b2ddbbf66d5d045678f6e11815b
Christian Nagel <christian@christiannagel.com>
2021-12-23 11:22:37 +0100

    styles - styles and resources #81

--- 4_Apps/Styles/StylesAndResources/StylesAndResources/ResourcesDemoUserControl.xaml.cs
+++ 4_Apps/Styles/StylesAndResources/StylesAndResources/ResourcesDemoUserControl.xaml.cs
@@ -1,40 +1,27 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace StylesAndResources;
 
-namespace StylesAndResources
+public sealed partial class ResourcesDemoUserControl : UserControl
 {
-    public sealed partial class ResourcesDemoUserControl : UserControl
+    public ResourcesDemoUserControl() => InitializeComponent();
+
+    private void OnApplyResources(object sender, RoutedEventArgs e)
     {
-        public ResourcesDemoUserControl()
+        var o = Application.Current.Resources["MyGradientBrush"];
+        if (sender is Control ctrl)
         {
-            this.InitializeComponent();
-        }
-
-        private void OnApplyResources(object sender, RoutedEventArgs e)
-        {
-            if (sender is Control ctrl)
+            if (ctrl.Background is null)
             {
-                object o = ctrl.FindName("MyGradientBrush");
-
-
-            
+                var brush = Application.Current.Resources["MyGradientBrush"] as Brush;
+                ctrl.Background = brush;
             }
-
+            else
+            {
+                ctrl.Background = null;
+            }
         }
     }
 }
--- 4_Apps/Styles/StylesAndResources/StylesAndResources/UseThemesUserControl.xaml.cs
+++ 4_Apps/Styles/StylesAndResources/StylesAndResources/UseThemesUserControl.xaml.cs
@@ -1,34 +1,15 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace StylesAndResources;
 
-namespace StylesAndResources
+public sealed partial class UseThemesUserControl : UserControl
 {
-    public sealed partial class UseThemesUserControl : UserControl
-    {
-        public UseThemesUserControl()
-        {
-            this.InitializeComponent();
-        }
+    public UseThemesUserControl() => InitializeComponent();
 
-        private void OnChangeTheme(object sender, RoutedEventArgs e)
-        {
-            grid1.RequestedTheme = grid1.RequestedTheme == ElementTheme.Dark ?
-                ElementTheme.Light : ElementTheme.Dark;
-        }
+    private void OnChangeTheme(object sender, RoutedEventArgs e)
+    {
+        grid1.RequestedTheme = grid1.RequestedTheme == ElementTheme.Dark ?
+            ElementTheme.Light : ElementTheme.Dark;
     }
 }
