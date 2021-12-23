921d40ace0cf3a829c32db13524c27c8e3a76187
Christian Nagel <christian@christiannagel.com>
2021-12-23 11:38:19 +0100

    styles - transformations #81

--- 4_Apps/Styles/Transformations/Transformations/SmilingFace.xaml.cs
+++ 4_Apps/Styles/Transformations/Transformations/SmilingFace.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Transformations;
 
-namespace Transformations
+public sealed partial class SmilingFace : UserControl
 {
-    public sealed partial class SmilingFace : UserControl
-    {
-        public SmilingFace() => InitializeComponent();
-    }
+    public SmilingFace() => InitializeComponent();
 }
