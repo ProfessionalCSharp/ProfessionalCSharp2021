cbb4a7e7f1f2d9acbc797d00c7206b86b83f47fa
Christian Nagel <christian@christiannagel.com>
2021-12-23 08:44:08 +0100

    windows apps - layout #81

--- 4_Apps/Windows/LayoutSamples/LayoutSamples/MainWindow.xaml.cs
+++ 4_Apps/Windows/LayoutSamples/LayoutSamples/MainWindow.xaml.cs
@@ -1,34 +1,27 @@
 ﻿using LayoutSamples.Views;
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Input;
-using System;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace LayoutSamples;
 
-namespace LayoutSamples
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow() => InitializeComponent();
+
+    private void OnNavigate(XamlUICommand sender, ExecuteRequestedEventArgs args)
     {
-        public MainWindow() => InitializeComponent();
-
-        private void OnNavigate(XamlUICommand sender, ExecuteRequestedEventArgs args)
+        Type pageType = args.Parameter switch
         {
-            Type pageType = args.Parameter switch
-            {
-                "Adaptive" => typeof(AdaptiveRelativePanelPage),
-                "Delay" => typeof(DelayLoadingPage),
-                "Grid" => typeof(GridPage),
-                "Relative" => typeof(RelativePanelPage),
-                "Stack" => typeof(StackPanelPage),
-                "Variable" => typeof(VariableSizedWrapGridPage),
-                _ => throw new InvalidOperationException()
-            };
+            "Adaptive" => typeof(AdaptiveRelativePanelPage),
+            "Delay" => typeof(DelayLoadingPage),
+            "Grid" => typeof(GridPage),
+            "Relative" => typeof(RelativePanelPage),
+            "Stack" => typeof(StackPanelPage),
+            "Variable" => typeof(VariableSizedWrapGridPage),
+            _ => throw new InvalidOperationException()
+        };
 
-            MainFrame.Navigate(pageType);
-        }
+        MainFrame.Navigate(pageType);
     }
 }
--- 4_Apps/Windows/LayoutSamples/LayoutSamples/Views/GridPage.xaml.cs
+++ 4_Apps/Windows/LayoutSamples/LayoutSamples/Views/GridPage.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace LayoutSamples.Views;
 
-namespace LayoutSamples.Views
+public sealed partial class GridPage : Page
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class GridPage : Page
-    {
-        public GridPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+    public GridPage() => InitializeComponent();
 }
