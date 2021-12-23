51dba6aed68b9bf638030730d859914493034e70
Christian Nagel <christian@christiannagel.com>
2021-12-23 09:39:22 +0100

    styles - shapes #81

--- 4_Apps/Styles/Shapes/Shapes/MainWindow.xaml.cs
+++ 4_Apps/Styles/Shapes/Shapes/MainWindow.xaml.cs
@@ -1,47 +1,41 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
+
 using Windows.Foundation;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Shapes;
 
-namespace Shapes
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow() => InitializeComponent();
+
+    private void OnChangeShape() => SetMouth();
+
+    private readonly Point[,] _mouthPoints = new Point[2, 3]
     {
-        public MainWindow() => InitializeComponent();
+        { new(40, 74), new(57, 95), new(80, 74) },
+        { new(40, 82), new(57, 65), new(80, 82) }
+    };
 
-        private void OnChangeShape() => SetMouth();
+    private bool _laugh = false;
+    public void SetMouth()
+    {
+        int index = _laugh ? 0 : 1;
 
-        private readonly Point[,] _mouthPoints = new Point[2, 3]
+        PathFigure figure = new() { StartPoint = _mouthPoints[index, 0] };
+        figure.Segments = new PathSegmentCollection();
+        QuadraticBezierSegment segment1 = new()
         {
-            { new(40, 74), new(57, 95), new(80, 74) },
-            { new(40, 82), new(57, 65), new(80, 82) }
+            Point1 = _mouthPoints[index, 1],
+            Point2 = _mouthPoints[index, 2]
         };
 
-        private bool _laugh = false;
-        public void SetMouth()
-        {
-            int index = _laugh ? 0 : 1;
+        figure.Segments.Add(segment1);
+        PathGeometry geometry = new();
+        geometry.Figures = new PathFigureCollection();
+        geometry.Figures.Add(figure);
 
-            PathFigure figure = new() { StartPoint = _mouthPoints[index, 0] };
-            figure.Segments = new PathSegmentCollection();
-            QuadraticBezierSegment segment1 = new()
-            {
-                Point1 = _mouthPoints[index, 1],
-                Point2 = _mouthPoints[index, 2]
-            };
-
-            figure.Segments.Add(segment1);
-            PathGeometry geometry = new();
-            geometry.Figures = new PathFigureCollection();
-            geometry.Figures.Add(figure);
-
-            mouth.Data = geometry;
-            _laugh = !_laugh;
-        }
+        mouth.Data = geometry;
+        _laugh = !_laugh;
     }
 }
