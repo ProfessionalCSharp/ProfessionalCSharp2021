d015c426d7d1cea424a429a1eea339fe33759905
Christian Nagel <christian@christiannagel.com>
2021-12-23 08:24:59 +0100

    windows apps - controls #81

--- 4_Apps/Windows/ControlsSamples/ControlsSamples/Views/ButtonsPage.xaml.cs
+++ 4_Apps/Windows/ControlsSamples/ControlsSamples/Views/ButtonsPage.xaml.cs
@@ -1,19 +1,14 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using System;
-using System.Runtime.InteropServices;
-using Windows.UI.Popups;
-using WinRT;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System.Runtime.InteropServices;
+
+using Windows.UI.Popups;
+
+using WinRT;
 
 namespace ControlsSamples.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ButtonsPage : Page
     {
         public ButtonsPage() => this.InitializeComponent();
