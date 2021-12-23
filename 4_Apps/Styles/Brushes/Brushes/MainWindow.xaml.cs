852736a12e5ee9e7da7251f7d25f2e3ee3aece92
Christian Nagel <christian@christiannagel.com>
2021-12-23 09:26:45 +0100

    styling - brushes #81

--- 4_Apps/Styles/Brushes/Brushes/MainWindow.xaml.cs
+++ 4_Apps/Styles/Brushes/Brushes/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Brushes;
 
-namespace Brushes
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow() => InitializeComponent();
-
-    }
+    public MainWindow() => InitializeComponent();
 }
