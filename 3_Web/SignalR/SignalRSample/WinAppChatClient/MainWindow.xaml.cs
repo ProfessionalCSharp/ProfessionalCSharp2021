490736c99a75d9cff844fac6be18e9afd256a8fd
Christian Nagel <christian@christiannagel.com>
2021-12-23 14:48:42 +0100

    signalr - winui client #81

--- 3_Web/SignalR/SignalRSample/WinAppChatClient/MainWindow.xaml.cs
+++ 3_Web/SignalR/SignalRSample/WinAppChatClient/MainWindow.xaml.cs
@@ -1,34 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using WinAppChatClient.Views;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using WindowsAppChatClient.ViewModels;
+﻿using Microsoft.UI.Xaml;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace WinAppChatClient;
 
-namespace WinAppChatClient
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();           
-        }
-    }
+    public MainWindow() => InitializeComponent();
 }
--- 3_Web/SignalR/SignalRSample/WinAppChatClient/ViewModels/GroupChatViewModel.cs
+++ 3_Web/SignalR/SignalRSample/WinAppChatClient/ViewModels/GroupChatViewModel.cs
@@ -1,128 +1,132 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Toolkit.Mvvm.Input;
-using System;
+
 using System.Collections.ObjectModel;
-using System.Net.Http;
-using System.Threading.Tasks;
 using System.Windows.Input;
+
 using WindowsAppChatClient.Services;
 
-namespace WindowsAppChatClient.ViewModels
+namespace WindowsAppChatClient.ViewModels;
+
+public sealed class GroupChatViewModel
 {
-    public sealed class GroupChatViewModel
+    private readonly IDialogService _dialogService;
+    private readonly UrlService _urlService;
+    public GroupChatViewModel(IDialogService dialogService, UrlService urlService)
     {
-        private readonly IDialogService _dialogService;
-        private readonly UrlService _urlService;
-        public GroupChatViewModel(IDialogService dialogService, UrlService urlService)
-        {
-            _dialogService = dialogService;
-            _urlService = urlService;
+        _dialogService = dialogService;
+        _urlService = urlService;
 
-            ConnectCommand = new RelayCommand(OnConnect);
-            SendCommand = new RelayCommand(OnSendMessage);
-            EnterGroupCommand = new RelayCommand(OnEnterGroup);
-            LeaveGroupCommand = new RelayCommand(OnLeaveGroup);
-        }
-
-        public string? Name { get; set; }
-        public string? Message { get; set; }
-        public string? NewGroup { get; set; }
-
-        public string? SelectedGroup { get; set; }
-
-        public ObservableCollection<string> Messages { get; } = new ObservableCollection<string>();
-        public ObservableCollection<string> Groups { get; } = new ObservableCollection<string>();
-
-        public ICommand SendCommand { get; }
-        public ICommand ConnectCommand { get; }
-        public ICommand EnterGroupCommand { get; }
-        public ICommand LeaveGroupCommand { get; }
-
-        private HubConnection? _hubConnection;
-
-        public async void OnConnect()
-        {
-            await CloseConnectionAsync();
-            _hubConnection = new HubConnectionBuilder()
-                .WithUrl(_urlService.GroupAddress)
-                .Build();
-
-            _hubConnection.Closed += HubConnectionClosed;
-
-            _hubConnection.On<string, string, string>("MessageToGroup", OnMessageReceived);
-
-            try
-            {
-                await _hubConnection.StartAsync();
-                await _dialogService.ShowMessageAsync("client connected");
-            }
-            catch (HttpRequestException ex)
-            {
-                await _dialogService.ShowMessageAsync(ex.Message);
-            }
-        }
-
-        public async void OnSendMessage()
-        {
-            try
-            {
-                await _hubConnection.SendAsync("Send", SelectedGroup, Name, Message);
-            }
-            catch (Exception ex)
-            {
-                await _dialogService.ShowMessageAsync(ex.Message);
-            }
-        }
-
-        public async void OnMessageReceived(string group, string name, string message)
-        {
-            try
-            {
-                Messages.Add($"{group}-{name}: {message}");
-            }
-            catch (Exception ex)
-            {
-                await _dialogService.ShowMessageAsync(ex.Message);
-            }
-        }
-
-        public async void OnEnterGroup()
-        {
-            try
-            {
-                if (NewGroup is not null)
-                {
-                    await _hubConnection.InvokeAsync("AddGroup", NewGroup);
-                    Groups.Add(NewGroup);
-                    SelectedGroup = NewGroup;
-                }
-            }
-            catch (Exception ex)
-            {
-                await _dialogService.ShowMessageAsync(ex.Message);
-            }
-        }
-
-        public async void OnLeaveGroup()
-        {
-            try
-            {
-                if (SelectedGroup is not null)
-                {
-                    await _hubConnection.InvokeAsync("LeaveGroup", SelectedGroup);
-                    Groups.Remove(SelectedGroup);
-                }
-            }
-            catch (Exception ex)
-            {
-                await _dialogService.ShowMessageAsync(ex.Message);
-            }
-        }
-
-        private Task HubConnectionClosed(Exception arg)
-            => _dialogService.ShowMessageAsync("Hub connection closed");
-
-        private ValueTask CloseConnectionAsync() =>
-            _hubConnection?.DisposeAsync() ?? ValueTask.CompletedTask;
+        ConnectCommand = new RelayCommand(OnConnect);
+        SendCommand = new RelayCommand(OnSendMessage);
+        EnterGroupCommand = new RelayCommand(OnEnterGroup);
+        LeaveGroupCommand = new RelayCommand(OnLeaveGroup);
     }
+
+    public string? Name { get; set; }
+    public string? Message { get; set; }
+    public string? NewGroup { get; set; }
+
+    public string? SelectedGroup { get; set; }
+
+    public ObservableCollection<string> Messages { get; } = new ObservableCollection<string>();
+    public ObservableCollection<string> Groups { get; } = new ObservableCollection<string>();
+
+    public ICommand SendCommand { get; }
+    public ICommand ConnectCommand { get; }
+    public ICommand EnterGroupCommand { get; }
+    public ICommand LeaveGroupCommand { get; }
+
+    private HubConnection? _hubConnection;
+
+    public async void OnConnect()
+    {
+        await CloseConnectionAsync();
+        _hubConnection = new HubConnectionBuilder()
+            .WithUrl(_urlService.GroupAddress)
+            .Build();
+
+        _hubConnection.Closed += HubConnectionClosed;
+
+        _hubConnection.On<string, string, string>("MessageToGroup", OnMessageReceived);
+
+        try
+        {
+            await _hubConnection.StartAsync();
+            await _dialogService.ShowMessageAsync("client connected");
+        }
+        catch (HttpRequestException ex)
+        {
+            await _dialogService.ShowMessageAsync(ex.Message);
+        }
+    }
+
+    public async void OnSendMessage()
+    {
+        if (_hubConnection is null) throw new InvalidOperationException("OnConnect needs to be invoked before OnSendMessage");
+
+        try
+        {
+            await _hubConnection.SendAsync("Send", SelectedGroup, Name, Message);
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowMessageAsync(ex.Message);
+        }
+    }
+
+    public async void OnMessageReceived(string group, string name, string message)
+    {
+        try
+        {
+            Messages.Add($"{group}-{name}: {message}");
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowMessageAsync(ex.Message);
+        }
+    }
+
+    public async void OnEnterGroup()
+    {
+        if (_hubConnection is null) throw new InvalidOperationException($"OnConnect needs to be invoked before {nameof(OnEnterGroup)}");
+
+        try
+        {
+            if (NewGroup is not null)
+            {
+                await _hubConnection.InvokeAsync("AddGroup", NewGroup);
+                Groups.Add(NewGroup);
+                SelectedGroup = NewGroup;
+            }
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowMessageAsync(ex.Message);
+        }
+    }
+
+    public async void OnLeaveGroup()
+    {
+        if (_hubConnection is null) throw new InvalidOperationException($"OnConnect needs to be invoked before {OnLeaveGroup}");
+
+        try
+        {
+            if (SelectedGroup is not null)
+            {
+                await _hubConnection.InvokeAsync("LeaveGroup", SelectedGroup);
+                Groups.Remove(SelectedGroup);
+            }
+        }
+        catch (Exception ex)
+        {
+            await _dialogService.ShowMessageAsync(ex.Message);
+        }
+    }
+
+    private Task HubConnectionClosed(Exception? arg)
+        => _dialogService.ShowMessageAsync("Hub connection closed");
+
+    private ValueTask CloseConnectionAsync() =>
+        _hubConnection?.DisposeAsync() ?? ValueTask.CompletedTask;
 }
