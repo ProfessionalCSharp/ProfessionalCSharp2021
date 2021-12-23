5448244b9ef815e3c39cf10de2da5187d3bb4c3c
Christian Nagel <christian@christiannagel.com>
2021-12-23 08:15:16 +0100

    Windows apps - data binding #81

--- 4_Apps/Windows/DataBindingSamples/DataBindingSamples/MainWindow.xaml.cs
+++ 4_Apps/Windows/DataBindingSamples/DataBindingSamples/MainWindow.xaml.cs
@@ -1,32 +1,25 @@
 ﻿using DataBindingSamples.Models;
 using DataBindingSamples.Services;
+
 using Microsoft.UI.Xaml;
+
 using System.Collections.ObjectModel;
-using System.Linq;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace DataBindingSamples
+namespace DataBindingSamples;
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        private readonly BooksService _booksService = new();
-        public MainWindow() => InitializeComponent();
+    private readonly BooksService _booksService = new();
+    public MainWindow() => InitializeComponent();
 
-        public ObservableCollection<Book> Books => _booksService.Books;
+    public ObservableCollection<Book> Books => _booksService.Books;
 
-        public void RefreshBooks() => _booksService.RefreshBooks();
+    public void RefreshBooks() => _booksService.RefreshBooks();
 
-        public void AddBook() =>
-          _booksService.AddBook(new Book(GetNextBookId(),
-            $"Professional C# and .NET - {GetNextYear()} Edition", "Wrox Press"));
+    public void AddBook() =>
+      _booksService.AddBook(new Book(GetNextBookId(),
+        $"Professional C# and .NET - {GetNextYear()} Edition", "Wrox Press"));
 
-        private int GetNextBookId() => Books.Select(b => b.BookId).Max() + 1;
-        private int _year = 2021;
-        private int GetNextYear() => _year += 3;
-    }
+    private int GetNextBookId() => Books.Select(b => b.BookId).Max() + 1;
+    private int _year = 2021;
+    private int GetNextYear() => _year += 3;
 }
--- 4_Apps/Windows/DataBindingSamples/DataBindingSamples/Models/Book.cs
+++ 4_Apps/Windows/DataBindingSamples/DataBindingSamples/Models/Book.cs
@@ -1,33 +1,33 @@
 ﻿using System.Collections.Generic;
 
-namespace DataBindingSamples.Models
+namespace DataBindingSamples.Models;
+
+public class Book : ObservableObject
 {
-    public class Book : ObservableObject
+    public Book(int id, string title, string publisher, params string[] authors)
     {
-        public Book(int id, string title, string publisher, params string[] authors)
-        {
-            BookId = id;
-            _title = title;
-            _publisher = publisher;
-            Authors = authors;
-        }
-        public int BookId { get; }
-        private string _title;
-
-        public string Title
-        {
-            get => _title;
-            set => SetProperty(ref _title, value);
-        }
-
-        private string _publisher;
-        public string Publisher
-        {
-            get => _publisher;
-            set => SetProperty(ref _publisher, value);
-        }
-        public IEnumerable<string> Authors { get; }
-
-        public override string ToString() => Title;
+        BookId = id;
+        _title = title;
+        _publisher = publisher;
+        Authors = authors;
     }
+
+    public int BookId { get; }
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+
+    private string _publisher;
+    public string Publisher
+    {
+        get => _publisher;
+        set => SetProperty(ref _publisher, value);
+    }
+    public IEnumerable<string> Authors { get; }
+
+    public override string ToString() => Title;
 }
