9bd978f65cc966ef33b1f26cdefc2497b0da382e
Christian Nagel <christian@christiannagel.com>
2022-02-11 11:26:15 +0100

    angular project update

--- 3_Web/ASPNETCore/AngularSample/ClientApp/proxy.conf.js
+++ 3_Web/ASPNETCore/AngularSample/ClientApp/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:29114';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:24553';
 
 const PROXY_CONFIG = [
   {
@@ -9,7 +9,10 @@ const PROXY_CONFIG = [
       "/weatherforecast",
    ],
     target: target,
-    secure: false
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
   }
 ]
 
