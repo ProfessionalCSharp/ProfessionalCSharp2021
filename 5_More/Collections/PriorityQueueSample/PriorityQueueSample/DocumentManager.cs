08a68f2537ff85a6dfbe6b4e317157aad756de0c
Christian Nagel <christian@christiannagel.com>
2022-04-02 20:33:57 +0200

    PriorityQueue sample

---
+++ 5_More/Collections/PriorityQueueSample/PriorityQueueSample/DocumentManager.cs
@@ -0,0 +1,35 @@
+﻿namespace PriorityQueueSample;
+
+public record Document(string Title, string Content, int Priority);
+
+public class DocumentManager
+{
+    private readonly PriorityQueue<Document, int> _documentQueue = new();
+
+    public void AddDocument(Document doc)
+    {
+        _documentQueue.Enqueue(doc, doc.Priority);
+    }
+    
+    public void DisplayAllNodesUnordered()
+    {       
+        foreach ((Document doc, _) in _documentQueue.UnorderedItems)
+        {
+            Console.WriteLine(doc);
+        }
+    }
+
+    public Document GetDocument()
+    {
+        return _documentQueue.Dequeue();
+    }
+
+    public void Run()
+    {
+        while (_documentQueue.Count >= 1)
+        {
+            var doc = _documentQueue.Dequeue();
+            Console.WriteLine(doc);
+        }
+    }
+}
---
+++ 5_More/Collections/PriorityQueueSample/PriorityQueueSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using PriorityQueueSample;
+
+DocumentManager dm = new();
+dm.AddDocument(new Document("one", "Sample", 8));
+dm.AddDocument(new Document("two", "Sample", 3));
+dm.AddDocument(new Document("three", "Sample", 4));
+dm.AddDocument(new Document("four", "Sample", 8));
+dm.AddDocument(new Document("five", "Sample", 1));
+dm.AddDocument(new Document("six", "Sample", 9));
+dm.AddDocument(new Document("seven", "Sample", 1));
+dm.AddDocument(new Document("eight", "Sample", 1));
+
+Console.WriteLine("All documents unordered");
+dm.DisplayAllNodesUnordered();
+Console.WriteLine();
+
+Console.WriteLine("dequeue all documents from the queue - ordered by priority");
+Console.WriteLine("Elements with the lowest priority are dequeued first");
+dm.Run();
