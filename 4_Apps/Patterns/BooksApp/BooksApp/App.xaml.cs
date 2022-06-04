11ea5209acf9d9136445588a7656d96a71e249f1
Christian Nagel <christian@christiannagel.com>
2022-06-04 08:57:20 +0200

    Windows App SDK 1.1 #107

--- 4_Apps/Patterns/BooksApp/BooksApp/App.xaml.cs
+++ 4_Apps/Patterns/BooksApp/BooksApp/App.xaml.cs
@@ -1,55 +1,53 @@
 ﻿
-
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace BooksApp
+namespace BooksApp;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
 {
     /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
-    public partial class App : Application
+    public App()
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
-            RegisterServices();
-
-            m_window = new MainWindow();
-            m_window.Activate();
-        }
-
-        private void RegisterServices()
-        {
-            Ioc.Default.ConfigureServices(
-                new ServiceCollection()
-                    .AddSingleton<IBooksRepository, BooksSampleRepository>()
-                    .AddScoped<BooksViewModel>()
-                    .AddScoped<BookDetailViewModel>()
-                    .AddScoped<MainWindowViewModel>()
-                    .AddSingleton<IItemsService<Book>, BooksService>()
-                    .AddSingleton<IDialogService, WinUIDialogService>()
-                    .AddSingleton<INavigationService, WinUINavigationService>()
-                    .AddSingleton<WinUIInitializeNavigationService>()
-                    .AddLogging(builder =>
-                    {
-                        builder.AddDebug();
-                    }).BuildServiceProvider());
-        }
-
-        private Window? m_window;
+        this.InitializeComponent();
     }
+
+    /// <summary>
+    /// Invoked when the application is launched normally by the end user.  Other entry points
+    /// will be used such as when the application is launched to open a specific file.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        RegisterServices();
+
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+
+    private void RegisterServices()
+    {
+        Ioc.Default.ConfigureServices(
+            new ServiceCollection()
+                .AddSingleton<IBooksRepository, BooksSampleRepository>()
+                .AddScoped<BooksViewModel>()
+                .AddScoped<BookDetailViewModel>()
+                .AddScoped<MainWindowViewModel>()
+                .AddSingleton<IItemsService<Book>, BooksService>()
+                .AddSingleton<IDialogService, WinUIDialogService>()
+                .AddSingleton<INavigationService, WinUINavigationService>()
+                .AddSingleton<WinUIInitializeNavigationService>()
+                .AddLogging(builder =>
+                {
+                    builder.AddDebug();
+                }).BuildServiceProvider());
+    }
+
+    private Window? m_window;
 }
