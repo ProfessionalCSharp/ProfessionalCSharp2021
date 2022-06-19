6772c9d572b8eeeb113c54fa08e42329338b1522
Christian Nagel <christian@christiannagel.com>
2022-06-19 21:42:58 +0200

    F1 G.P Canada 2022

--- 1_CS/LINQ/DataLib/Formula1.cs
+++ 1_CS/LINQ/DataLib/Formula1.cs
@@ -34,13 +34,13 @@ public static class Formula1
         new("Jacques", "Villeneuve", "Canada", 165, 11, new int[] { 1997 }, new string[] { "Williams" }),
         new("Mika", "Hakkinen", "Finland", 160, 20, new int[] { 1998, 1999 }, new string[] { "McLaren" }),
         new("Michael", "Schumacher", "Germany", 287, 91, new int[] { 1994, 1995, 2000, 2001, 2002, 2003, 2004 }, new string[] { "Benetton", "Ferrari" }),
-        new("Fernando", "Alonso", "Spain", 344, 32, new int[] { 2005, 2006 }, new string[] { "Renault" }),
+        new("Fernando", "Alonso", "Spain", 345, 32, new int[] { 2005, 2006 }, new string[] { "Renault" }),
         new("Kimi", "Räikkönen", "Finland", 352, 21, new int[] { 2007 }, new string[] { "Ferrari" }),
         new("Jenson", "Button", "UK", 306, 16, new int[] { 2009 }, new string[] { "Brawn GP" }),
-        new("Sebastian", "Vettel", "Germany", 286, 53, new int[] { 2010, 2011, 2012, 2013 }, new string[] { "Red Bull Racing" }),
+        new("Sebastian", "Vettel", "Germany", 287, 53, new int[] { 2010, 2011, 2012, 2013 }, new string[] { "Red Bull Racing" }),
         new("Nico", "Rosberg", "Germany", 207, 24, new int[] { 2016 }, new string[] { "Mercedes" }),
-        new("Lewis", "Hamilton", "UK", 296, 103, new int[] { 2008, 2014, 2015, 2017, 2018, 2019, 2020 }, new string[] { "McLaren", "Mercedes" }),
-        new("Max", "Verstappen", "Netherlands", 149, 25, new int[] { 2021 }, new string[] { "Red Bull Racing" })
+        new("Lewis", "Hamilton", "UK", 297, 103, new int[] { 2008, 2014, 2015, 2017, 2018, 2019, 2020 }, new string[] { "McLaren", "Mercedes" }),
+        new("Max", "Verstappen", "Netherlands", 150, 26, new int[] { 2021 }, new string[] { "Red Bull Racing" })
     };
 
     private static List<Team>? s_teams;
@@ -174,7 +174,7 @@ public static class Formula1
         new Racer("Juan Pablo", "Montoya", "Columbia", Starts: 94, Wins: 7),
         new Racer("Felipe", "Massa", "Brazil", Starts: 269, Wins: 11),
         new Racer("Mark", "Webber", "Australia", Starts: 215, Wins: 9),
-        new Racer("Daniel", "Ricciardo", "Australia", Starts: 218, Wins: 8),
-        new Racer("Valtteri", "Bottas", "Finland", Starts: 186, Wins: 10)
+        new Racer("Daniel", "Ricciardo", "Australia", Starts: 219, Wins: 8),
+        new Racer("Valtteri", "Bottas", "Finland", Starts: 187, Wins: 10)
     };
 }
