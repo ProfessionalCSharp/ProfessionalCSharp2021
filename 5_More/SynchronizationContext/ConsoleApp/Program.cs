f1044548a6747858e7262e9d7f0f8d0f1b227d2e
Christian Nagel <christian@christiannagel.com>
2022-08-27 10:14:54 +0200

    console app showing null synchronizationcontext

---
+++ 5_More/SynchronizationContext/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿SynchronizationContext? context = SynchronizationContext.Current;
+if (context == null)
+{
+    Console.WriteLine("No synchronization context with console applications");
+}
+
+ShowThreadAndTask("Started application");
+await SampleAsync();
+
+ShowThreadAndTask("Top level statements after calling SampleAsync");
+
+Task SampleAsync()
+{
+    return Task.Run(() =>
+    {
+        ShowThreadAndTask("Started AsyncMethod");
+    });
+}
+
+void ShowThreadAndTask(string title)
+{
+    Console.WriteLine(title);
+    string taskMessage = Task.CurrentId == null ? "no task" : $"in the task {Task.CurrentId}";
+    Console.WriteLine($"running in thread {Environment.CurrentManagedThreadId} and {taskMessage}");
+    Console.WriteLine();
+}
