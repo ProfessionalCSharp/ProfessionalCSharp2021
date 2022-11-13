d8071a64705a307bacf8c9cae325da67b0cae102
Christian Nagel <christian@christiannagel.com>
2022-11-13 16:02:22 +0100

    async streaming with ASP.NET Core

---
+++ 5_More/Services/ASPNETCoreAsyncStreaming/ASPNETCoreAPIAsyncStreaming/Controllers/ADeviceController.cs
@@ -0,0 +1,20 @@
+﻿using AsyncStreaming.Shared;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreAPIAsyncStreaming.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ADeviceController : ControllerBase
+{
+    [HttpGet]
+    public async IAsyncEnumerable<DeviceData> GetSomeData()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            yield return new DeviceData($"text {Random.Shared.Next(200)}", DateTime.Now, i);
+            await Task.Delay(100);
+        }
+    }
+}
