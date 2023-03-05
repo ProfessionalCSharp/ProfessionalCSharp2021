3654bf5e3b484779eeab4018b17a95073160a2e6
Christian Nagel <christian@christiannagel.com>
2023-03-05 22:05:57 +0100

    sample with EF Core tph mapping with conversions, and implementing IParsable

---
+++ 5_More/EFCore/InhertianceMappingWithConversion/GameData.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Codebreaker.Models;
+
+public record class GameData(Guid GameId, string GameType, string PlayerName)
+{
+    public ICollection<MoveData> Moves { get;  } = new List<MoveData>();
+}
+
+public abstract record class MoveData(Guid GameId, Guid MoveId, int MoveNumber);
+public record class MoveData<TField>(Guid GameId, Guid MoveId, int MoveNumber)
+    : MoveData(GameId, MoveId, MoveNumber)
+{
+    public required ICollection<TField> Fields { get; init; }
+}
+
+public record ColorField(string Color) : IParsable<ColorField>
+{
+    public override string ToString() => Color;
+
+    public static ColorField Parse(string s, IFormatProvider? provider)
+    {
+        if (TryParse(s, null, out ColorField? color))
+        {
+            return color;   
+        }
+        else
+        {
+            throw new ArgumentException($"Cannot parse value {s}", nameof(s));
+        }
+    }
+
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ColorField result)
+    {
+        if (s is null)
+        {
+            result = null;
+            return false;
+        }
+        result = new ColorField(s);
+        return true;
+    }
+}
+public record ShapeAndColorField(string Shape, string Color) : IParsable<ShapeAndColorField>
+{
+    public override string ToString() => $"{Shape};{Color}";
+
+    public static ShapeAndColorField Parse(string s, IFormatProvider? provider)
+    {
+        if (TryParse(s, provider, out ShapeAndColorField? shape))
+        {
+            return shape;
+        }
+        else
+        {
+            throw new ArgumentException($"Cannot parse value {s}", nameof(s));
+        }
+    }
+
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ShapeAndColorField result)
+    {
+        result = null;
+        if (s is null)
+        {
+            return false;
+        }
+        string[] parts = s.Split(';');
+        if (parts.Length != 2)
+        {
+            return false;
+        }
+        result = new ShapeAndColorField(parts[0], parts[1]);
+        return true;
+    }
+}
---
+++ 5_More/EFCore/InhertianceMappingWithConversion/Program.cs
@@ -0,0 +1,91 @@
+﻿using Codebreaker.Models;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSqlServer<GamesContext>(builder.Configuration.GetConnectionString("GamesConnection") ?? throw new InvalidOperationException());
+
+var app = builder.Build();
+
+var gameId1 = Guid.NewGuid();
+var gameId2 = Guid.NewGuid();
+
+{
+    // create two different games
+    using var gameScope = app.Services.CreateScope();
+    var context = gameScope.ServiceProvider.GetRequiredService<GamesContext>();
+
+    await context.Database.EnsureCreatedAsync();
+
+    GameData gameData1 = new(gameId1, "6x4", "player1");
+    GameData gameData2 = new GameData(gameId2, "5x5x4", "player2");
+
+    context.Games.Add(gameData1);
+    context.Games.Add(gameData2);
+    await context.SaveChangesAsync();
+}
+
+{
+    // add moves to the games
+    using var moveScope = app.Services.CreateScope();
+    var context = moveScope.ServiceProvider.GetRequiredService<GamesContext>();
+
+    MoveData<ColorField> moveData1 = new(gameId1, Guid.NewGuid(), 1)
+    {
+        Fields = new List<ColorField>()
+        {
+            new("red"),
+            new("blue"),
+            new("green"),
+            new("yellow")
+        }
+    };
+
+    MoveData<ShapeAndColorField> moveData2 = new(gameId2, Guid.NewGuid(), 1)
+    {   
+        Fields = new List<ShapeAndColorField>()
+        {
+            new("circle", "red"),
+            new("square", "blue"),
+            new("triangle", "green"),
+            new("circle", "yellow"),
+        }
+    };
+
+    var game1 = await context.Games.SingleAsync(g => g.GameId == gameId1);
+    game1.Moves.Add(moveData1);
+
+    var game2 = await context.Games.SingleAsync(g => g.GameId == gameId2);
+    game2.Moves.Add(moveData2);
+
+    await context.SaveChangesAsync();   
+}
+
+{
+    // read the games and moves
+    using var gamesscope = app.Services.CreateScope();
+    var context = gamesscope.ServiceProvider.GetRequiredService<GamesContext>();
+    var games = await context.Games
+        .Include(g => g.Moves).ToListAsync();
+    foreach ( var game in games )
+    {
+        Console.WriteLine(game.GameType);
+        foreach (var move in game.Moves)
+        {
+            Console.WriteLine($"move type: {TypeInforation(move)}");       
+        }
+    }
+}
+
+string TypeInforation(MoveData moveData)
+{
+    return moveData switch
+    {
+        MoveData<ColorField> => "MoveData<ColorField>",
+        MoveData<ShapeAndColorField> => "MoveData<ShapeAndColorField>",
+        _ => "unknown"
+    };
+}
