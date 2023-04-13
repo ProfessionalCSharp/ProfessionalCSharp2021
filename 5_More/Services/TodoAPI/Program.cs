e7e63cd1cc18ca284e6405beefe164343adf7efa
Christian Nagel <christian@christiannagel.com>
2023-04-13 20:08:08 +0200

    initial .NET 8 todo api

---
+++ 5_More/Services/TodoAPI/Program.cs
@@ -0,0 +1,18 @@
+using TodoAPI;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
