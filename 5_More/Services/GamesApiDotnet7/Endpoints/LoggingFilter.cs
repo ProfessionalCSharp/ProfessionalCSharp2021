77754d89e16533b212e05c554997a150e98e59fb
Christian Nagel <christian@christiannagel.com>
2023-05-02 20:47:13 +0200

    .net 7 updates minimal api samples

---
+++ 5_More/Services/GamesApiDotnet7/Endpoints/LoggingFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+
+namespace Codebreaker.Endpoints;
+
+public class LoggingFilter : IEndpointFilter
+{
+    private readonly ILogger _logger;
+    public LoggingFilter(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<LoggingFilter>();
+    }
+
+    public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+    {
+        _logger.LogTrace("{uri} called using {method}", context.HttpContext.Request.GetDisplayUrl(), context.HttpContext.Request.Method);
+        return await next(context);
+    }
+}
