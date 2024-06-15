23edb8fb25cd4719c38b783ed46198926eb6f637
Christian Nagel <christian@christiannagel.com>
2024-06-15 20:29:48 +0200

    .net 8 angular updates ch24

--- 3_Web/ASPNETCore/AngularSample/ClientApp/proxy.conf.js
+++ 3_Web/ASPNETCore/AngularSample/ClientApp/proxy.conf.js
@@ -1,13 +1,14 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:4060';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:43600';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/weatherforecast",
    ],
+    proxyTimeout: 10000,
     target: target,
     secure: false,
     headers: {
