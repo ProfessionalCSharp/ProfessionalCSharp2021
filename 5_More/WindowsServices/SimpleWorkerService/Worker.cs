1fc99309f18fcd8f316e2c0c5f6e7177d13b510b
Christian Nagel <christian@christiannagel.com>
2024-10-25 21:27:33 +0200

    update windows service to .net 8

--- 5_More/WindowsServices/SimpleWorkerService/Worker.cs
+++ 5_More/WindowsServices/SimpleWorkerService/Worker.cs
@@ -1,22 +1,15 @@
 namespace SimpleWorkerService;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-
-    public Worker(ILogger<Worker> logger, IHostEnvironment environment)
-    {
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         string[] args = Environment.GetCommandLineArgs();
-        _logger.LogInformation("command line args: {args}", string.Join(" ", args));
+        logger.LogInformation("command line args: {args}", string.Join(" ", args));
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(10000, stoppingToken);
         }
     }
