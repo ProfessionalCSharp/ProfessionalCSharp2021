9324fcda43eb4ffb545f081243c3e8e551f407c3
Christian Nagel <christian@christiannagel.com>
2024-10-25 13:16:45 +0200

    dotnet 9 with render mode display

---
+++ 5_More/Blazor/BlazorUnitedSampleDotnet9/BlazorUnitedSample.Client/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorUnitedSample.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string? Publisher { get; set; }
+}
---
+++ 5_More/Blazor/BlazorUnitedSampleDotnet9/BlazorUnitedSample.Client/Services/BooksAPIClient.cs
@@ -0,0 +1,32 @@
+﻿using BlazorUnitedSample.Models;
+using BlazorUnitedSample.Services;
+
+using System.Net.Http.Json;
+
+namespace BlazorUnitedSample.Client.Services;
+
+public class BooksAPIClient(HttpClient client) : IBooksService
+{
+    public async Task<Book> AddBookAsync(Book book)
+    {
+        await client.PostAsJsonAsync("/api/Book", book);
+        return book;
+    }
+
+    public async Task<Book?> GetBookByIdAsync(int id)
+    {
+        var book = await client.GetFromJsonAsync<Book>($"/api/Book/{id}");
+        return book;
+    }
+
+    public async Task<IEnumerable<Book>> GetBooksAsync()
+    {
+        var books = await client.GetFromJsonAsync<IEnumerable<Book>>("/api/Book");
+        return books ?? [];
+    }
+
+    public Task<int> UpdateBookAsync(int id, Book book)
+    {
+        throw new NotImplementedException();
+    }
+}
