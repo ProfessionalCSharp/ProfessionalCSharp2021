31004d30a2684b18301f7a55bc8be795ec1cbc02
Christian Nagel <christian@christiannagel.com>
2025-02-09 10:49:55 +0100

    .net 8 - types #203

--- 1_CS/Types/ClassesSample/Person.cs
+++ 1_CS/Types/ClassesSample/Person.cs
@@ -1,14 +1,8 @@
-public class Person
-{ 
-    public Person(string firstName, string lastName)
-    {
-        _firstName = firstName;
-        _lastName = lastName;
-    }
-
-    private readonly string _firstName;
+public class Person(string firstName, string lastName)
+{
+    private readonly string _firstName = firstName;
     public string FirstName => _firstName;
-    private readonly string _lastName;
+    private readonly string _lastName = lastName;
     public string LastName => _lastName;
 
     public string FullName => $"{FirstName} {LastName}";
