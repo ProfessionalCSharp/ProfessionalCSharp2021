fba2f9409d03be7411cab1b2279ab76a79f52a7a
Christian Nagel <christian@christiannagel.com>
2025-05-08 09:39:25 +0200

    new service discovery sample

---
+++ 5_More/Network/ServiceDiscoverySample/ServiceDiscoverySample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ServiceDiscovery;
+
+using ServiceDiscoverySample;
+
+Console.WriteLine("client - wait for service");
+Console.ReadLine();
+
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddConfigurationServiceEndpointProvider();
+builder.Services.AddDnsServiceEndpointProvider();
+builder.Services.AddDnsSrvServiceEndpointProvider();
+
+builder.Services.Configure<ServiceDiscoveryOptions>(options =>
+{
+    
+});
+builder.Services.AddServiceDiscovery();
+
+builder.Services.AddHttpClient<Runner>(client =>
+{
+    client.BaseAddress = new Uri("https+http://weather");
+}).AddServiceDiscovery();
+
+var app = builder.Build();
+var runner = app.Services.GetRequiredService<Runner>();
+
+await runner.ShowWeatherAsync();
---
+++ 5_More/Network/ServiceDiscoverySample/ServiceDiscoverySample/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+
+namespace ServiceDiscoverySample;
+
+public class Runner(HttpClient client)
+{
+    public async Task ShowWeatherAsync()
+    {
+        var weatherList = await client.GetFromJsonAsync<IEnumerable<WeatherForecast>>("/weatherforecast") ?? throw new InvalidOperationException();
+
+        foreach (var weather in weatherList)
+        {
+            Console.WriteLine(weather); 
+        }
+    }
+}
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
